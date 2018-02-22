4f2655d7d0c7d107c9d9c90dbc691d302b6282dd
Benoit Bouchaud <benbouchaud@gmail.com>
2018-02-22 15:42:01 +0100

    Updated fields + page.php

--- page.php
+++ page.php
@@ -1,12 +1,24 @@
 <?php
 
 $context = Timber::get_context();
+
+// Creating Timber object to access twig keys
 $context['post'] = new TimberPost();
+
+// Creating object Timber Image for hero_image
 $context['post']->img = new TimberImage($post->hero_img);
+
+// Declare woody_parts
 $context['post']->woody_parts = [];
 
+// Get main menu
+// $context['main_menu'] = new Timber\Menu('main-menu');
+// d($context['main_menu']);
+
 // We get all layouts used on the page and removed duplicates
-$content_element_layouts = array_unique($context['post']->content_element);
+if(!empty($context['post']->content_element)){
+    $content_element_layouts = array_unique($context['post']->content_element);
+}
 foreach ($content_element_layouts as $key => $layout) {
     // Then, for each layout we get twig's templates paths
     $woody = new Woody($layout);
