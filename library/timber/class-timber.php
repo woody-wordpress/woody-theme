03731ae17c04184af9b606a37061362a975aa017
Léo POIROUX <leo.poiroux@gmail.com>
2018-06-05 18:42:26 +0200

    Disable Timber

--- library/timber/class-timber.php
+++ library/timber/class-timber.php
@@ -1,47 +1,47 @@
 <?php
 
-class HawwwaiTheme_Timber extends TimberSite
-{
+// class HawwwaiTheme_Timber extends TimberSite
+// {
 
-    // Adding functionality to Twig
-    public function __construct()
-    {
-        parent::__construct();
-        $this->register_hooks();
-    }
+//     // Adding functionality to Twig
+//     public function __construct()
+//     {
+//         parent::__construct();
+//         $this->register_hooks();
+//     }
 
-    protected function register_hooks()
-    {
-        // add_filter( 'timber/twig', array( $this, 'basetheme_add_to_twig'));
-        // add_filter('timber/context', array($this, 'add_to_context'));
-    }
+//     protected function register_hooks()
+//     {
+//         // add_filter( 'timber/twig', array( $this, 'basetheme_add_to_twig'));
+//         // add_filter('timber/context', array($this, 'add_to_context'));
+//     }
 
-    // Global context, available to all templates
-    public function add_to_context($context)
-    {
+//     // Global context, available to all templates
+//     public function add_to_context($context)
+//     {
 
-        // WP Templates
-        // $context['wp']['template'] = array(
-        //     'front_page' => is_front_page(),
-        //     'blog' => is_home(),
-        // );
+//         // WP Templates
+//         // $context['wp']['template'] = array(
+//         //     'front_page' => is_front_page(),
+//         //     'blog' => is_home(),
+//         // );
 
-        // $context['wp']['theme'] = array(
-        //     'theme_mod' => get_theme_mod('wpt_mobile_menu_layout')
-        // );
+//         // $context['wp']['theme'] = array(
+//         //     'theme_mod' => get_theme_mod('wpt_mobile_menu_layout')
+//         // );
 
-        // //Menus
-        // $context['wp']['menus'] = array(
-        //   "main" => new Timber\Menu('main'),
-        //   "footer" => new Timber\Menu('footer'),
-        // );
+//         // //Menus
+//         // $context['wp']['menus'] = array(
+//         //   "main" => new Timber\Menu('main'),
+//         //   "footer" => new Timber\Menu('footer'),
+//         // );
 
-        // $context['main_menu'] = new Timber\Menu('main-menu');
-        // d($context['main_menu']);
+//         // $context['main_menu'] = new Timber\Menu('main-menu');
+//         // d($context['main_menu']);
 
-        return $context;
-    }
-}
+//         return $context;
+//     }
+// }
 
-// Execute Class
-$HawwwaiTheme_Timber = new HawwwaiTheme_Timber();
+// // Execute Class
+// $HawwwaiTheme_Timber = new HawwwaiTheme_Timber();
