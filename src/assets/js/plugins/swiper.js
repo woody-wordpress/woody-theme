8d46e492e7695f08bc585f2321355f341b881815
Benoit Bouchaud <benbouchaud@gmail.com>
2018-06-13 18:04:09 +0200

    Updated swiper

--- src/assets/js/plugins/swiper.js
+++ src/assets/js/plugins/swiper.js
@@ -1,43 +1,29 @@
+import $ from 'jquery';
 import Swiper from 'swiper';
 
-var woodySwiperShow1 = new Swiper('.swiper-container', {
-    // Optional parameters
-    keyboard: {
-        enabled: true,
-    },
+$('.swiper-container').each(function() {
+    var $this = $(this);
+    var slidesPerView = $this.data('slides-per-view');
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        dynamicBullets: true,
-        clickable: true
-    },
+    new Swiper($this, {
+        // Optional parameters
+        slidesPerView: slidesPerView,
+        spaceBetween: 30,
+        keyboard: {
+            enabled: true,
+        },
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            dynamicBullets: true,
+            clickable: true
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 });
-
-var woodySwiperShow2 = new Swiper('.swiper-container.slidesPerView-3', {
-    // Optional parameters
-    slidesPerView: 3,
-    spaceBetween: 30,
-    keyboard: {
-        enabled: true,
-    },
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        dynamicBullets: true,
-        clickable: true
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-})
