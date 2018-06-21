e330441291e93560319f2a1794cbf718ee8b6359
Benoit Bouchaud <benbouchaud@gmail.com>
2018-06-21 16:49:46 +0200

    Swiper js + changed name of en-tête de page

--- src/assets/js/plugins/swiper.js
+++ src/assets/js/plugins/swiper.js
@@ -3,16 +3,40 @@ import Swiper from 'swiper';
 
 $('.swiper-container').each(function() {
     var $this = $(this);
-    var slidesPerView = $this.data('slides-per-view');
-    if (slidesPerView > 1) {
-        var spaceBetween = 30;
+
+    if (typeof($this.data('slides-per-view')) !== undefined) {
+        var slidesPerView = $this.data('slides-per-view');
     } else {
-        var spaceBetween = $this.data('slides-spacing');
+        var slidesPerView = 1;
     }
+
+    if (typeof($this.data('slides-spacing')) !== undefined) {
+        var spaceBetween = $this.data('slides-spacing');
+    } else {
+        var spaceBetween = 30;
+    }
+
+    if (typeof($this.data('slides-loop')) !== undefined) {
+        var loop = $this.data('slides-loop');
+    } else {
+        var loop = false;
+    }
+
+    if (typeof($this.data('slides-center')) !== undefined) {
+        var centeredSlides = $this.data('slides-center');
+    } else {
+        var centeredSlides = false;
+    }
+
+    console.info(spaceBetween);
+
     new Swiper($this, {
         // Optional parameters
         slidesPerView: slidesPerView,
         spaceBetween: spaceBetween,
+        freeMode: true,
+        loop: loop,
+        centeredSlides: centeredSlides,
         keyboard: {
             enabled: true,
         },
