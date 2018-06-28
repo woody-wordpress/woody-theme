cb75c01adf39a33272dc452bb896c33814b5758c
Benoit Bouchaud <benbouchaud@gmail.com>
2018-06-28 14:12:57 +0200

    little fixes

--- src/assets/js/admin/hawwwai-popup.js
+++ src/assets/js/admin/hawwwai-popup.js
@@ -2,26 +2,27 @@ import $ from 'jquery';
 
 $('.acf-field-5b33902f31b18').click(function() {
     var $this = $(this),
+        // On récupère la valeur du champ conf_id pour la passer à l'éditeur le cas échéant
+        conf_id = $this.siblings('.acf-field-5b338ff331b17').find('input[type="text"]').attr('value'),
         // On vérifie si l'élément et enfant du champ 'section' (acf-field-5afd2c6916ecb) pour définir la variable de contexte
         field_parents = $this.parents(),
-        is_section = field_parents.find('.acf-field-5afd2c6916ecb').length,
-        // On récupère la valeur du champ conf_id pour la passer à l'éditeur le cas échéant
-        conf_id = $this.siblings('.acf-field-5b338ff331b17').find('input[type="text"]').attr('value');
+        is_section = field_parents.find('.acf-field-5afd2c6916ecb').length;
 
     if (is_section == 1) {
         var context = 'playlist_block';
+        var playlist_name = '';
     } else {
-        var context = 'playlist_page';
+        var context = 'playlist_page',
+            page_title = $('#post-body input[name="post_title"]').attr('value'),
+            playlist_name = 'WP - Playlist ' + page_title;
     }
 
     // Log des résultats des variables : OK
-    console.info('Contexte : ' + context + ', conf_id : ' + conf_id);
+    console.info('Contexte : ' + context + ', conf_id : ' + conf_id + ', Playlist name : ' + playlist_name);
 
-    // Construction de l'iframe
+    // Construction et affichage de l'iframe de l'éditeur de playlist
     var editorUrl = 'https://monyssb.com/',
         iframe = '<div class="playlist-editor" data-role="popup"><iframe src="' + editorUrl + '"><p>Your browser does not support iframes.</p></iframe></div>';
-
     $('#acf-group_5b33890e6fa0b').append(iframe);
-    // $('.playlist-editor').addClass('active');
 
 });
