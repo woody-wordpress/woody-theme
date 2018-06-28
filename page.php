5955887856e21a9568e937787066e7f5c960e1a1
Benoit Bouchaud <benbouchaud@gmail.com>
2018-06-28 15:22:06 +0200

    some fixes

--- page.php
+++ page.php
@@ -36,60 +36,70 @@ $page_heading_tpl = get_field('field_5b052d70ea19b');
 
 $context['page_heading'] = Timber::compile($context['woody_components'][$page_heading_tpl], $page_heading);
 
-/** ************************
- * Compilation des sections
- ************************ **/
-rcd($context['post']->get_field('section'), true);
-$context['sections'] = [];
-$sections = $context['post']->get_field('section');
 
-// Foreach section, fill vars to display in the woody's components
-foreach ($sections as $key => $section) {
+
+$page_type_term = wp_get_post_terms($context['post']->ID, 'page_type');
+$page_type = $page_type_term[0]->slug;
+
+if ($page_type === 'playlist_tourism') {
+    // TODO : Renvoyer vers la fonction d'appel à l'API render
+} else {
+    /** ************************
+    * Compilation des sections
+    ************************ **/
+
+    $context['sections'] = [];
+    $sections = $context['post']->get_field('section');
+
+    if (!empty($sections)) {
+        // Foreach section, fill vars to display in the woody's components
+        foreach ($sections as $key => $section) {
 
     // On compile les données du header de section
-    $the_header = Timber::compile($context['woody_components']['section-section_header-tpl_1'], $section);
+            $the_header = Timber::compile($context['woody_components']['section-section_header-tpl_1'], $section);
 
-    // On compile les données du footer de section
-    $the_footer = Timber::compile($context['woody_components']['section-section_footer-tpl_1'], $section);
+            // On compile les données du footer de section
+            $the_footer = Timber::compile($context['woody_components']['section-section_footer-tpl_1'], $section);
 
-    // Pour chaque bloc d'une section, on compile les données dans un template Woody
-    // Puis on les compile dans le template de grille Woody selectionné
-    $components = [];
+            // Pour chaque bloc d'une section, on compile les données dans un template Woody
+            // Puis on les compile dans le template de grille Woody selectionné
+            $components = [];
 
-    if (!empty($section['section_content'])) {
-        foreach ($section['section_content'] as $key => $layout) {
-            if ($layout['acf_fc_layout'] == 'manual_focus') {
-                $the_items = getManualFocus_data($layout['content_selection']);
-                $components['items'][] = Timber::compile($context['woody_components'][$layout['woody_tpl']], $the_items);
-            } elseif ($layout['acf_fc_layout'] == 'auto_focus') {
-                $the_items = getAutoFocus_data($context['post'], $layout);
-                $components['items'][] = Timber::compile($context['woody_components'][$layout['woody_tpl']], $the_items);
-            } elseif ($layout['acf_fc_layout'] == 'playlist_bloc') {
-                // TODO : Renvoyer vers la fonction d'appel à l'API render
-                print 'Ceci est une playlist d\'objets touristiques';
-            } else {
-                $components['items'][] = Timber::compile($context['woody_components'][$layout['woody_tpl']], $layout);
+            if (!empty($section['section_content'])) {
+                foreach ($section['section_content'] as $key => $layout) {
+                    if ($layout['acf_fc_layout'] == 'manual_focus') {
+                        $the_items = getManualFocus_data($layout['content_selection']);
+                        $components['items'][] = Timber::compile($context['woody_components'][$layout['woody_tpl']], $the_items);
+                    } elseif ($layout['acf_fc_layout'] == 'auto_focus') {
+                        $the_items = getAutoFocus_data($context['post'], $layout);
+                        $components['items'][] = Timber::compile($context['woody_components'][$layout['woody_tpl']], $the_items);
+                    } elseif ($layout['acf_fc_layout'] == 'playlist_bloc') {
+                        // TODO : Renvoyer vers la fonction d'appel à l'API render
+                    } else {
+                        $components['items'][] = Timber::compile($context['woody_components'][$layout['woody_tpl']], $layout);
+                    }
+                }
+
+                if (!empty($section['woody_tpl'])) {
+                    $the_layout = Timber::compile($context['woody_components'][$section['woody_tpl']], $components);
+                }
             }
-        }
 
-        if (!empty($section['woody_tpl'])) {
-            $the_layout = Timber::compile($context['woody_components'][$section['woody_tpl']], $components);
-        }
-    }
+            // On récupère les données d'affichage personnalisables
+            $display = getDisplayOptions($section);
 
-    // On récupère les données d'affichage personnalisables
-    $display = getDisplayOptions($section);
-
-    // On ajoute les 3 parties compilées d'une section + ses paramètres d'affichage
-    // puis on compile le tout dans le template de section Woody
-    $the_section = [
+            // On ajoute les 3 parties compilées d'une section + ses paramètres d'affichage
+            // puis on compile le tout dans le template de section Woody
+            $the_section = [
         'header' => $the_header,
         'footer' => $the_footer,
         'layout' => $the_layout,
         'display' => $display,
     ];
 
-    $context['the_sections'][] = Timber::compile($context['woody_components']['section-section_full-tpl_1'], $the_section);
+            $context['the_sections'][] = Timber::compile($context['woody_components']['section-section_full-tpl_1'], $the_section);
+        }
+    }
 }
 
 // Render the $context in page.twig
