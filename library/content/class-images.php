9999a8bb2057080e14442deebd5e7855d1a9ad5f
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2018-07-20 14:52:06 +0200

    New images sizes

--- library/content/class-images.php
+++ library/content/class-images.php
@@ -83,50 +83,56 @@ class WoodyTheme_Images
         //     ));
         // });
 
-        // Ratio 8:1 => Panoramique
+        // Ratio 8:1 => Panoramique 1
         add_image_size('ratio_8_1_small', 360, 45, true);
         add_image_size('ratio_8_1_medium', 640, 80, true);
         add_image_size('ratio_8_1', 1200, 150, true);
         add_image_size('ratio_8_1_xlarge', 1920, 240, true);
 
-        // Ratio 4:1 => Panoramique moyen
+        // Ratio 4:1 => Panoramique 2
         add_image_size('ratio_4_1_small', 360, 90, true);
         add_image_size('ratio_4_1_medium', 640, 160, true);
         add_image_size('ratio_4_1', 1200, 300, true);
         add_image_size('ratio_4_1_xlarge', 1920, 480, true);
 
-        // Ratio 8:3 => Paysage long
-        add_image_size('ratio_8_3_small', 360, 135, true);
-        add_image_size('ratio_8_3_medium', 640, 240, true);
-        add_image_size('ratio_8_3', 1200, 450, true);
-        add_image_size('ratio_8_3_xlarge', 1920, 720, true);
+        // Ratio 2:1 => Paysage 1
+        add_image_size('ratio_2_1_small', 360, 180, true);
+        add_image_size('ratio_2_1_medium', 640, 320, true);
+        add_image_size('ratio_2_1', 1200, 600, true);
+        add_image_size('ratio_2_1_xlarge', 1920, 960, true);
 
-        // Ratio 16:9 => Paysage
-        add_image_size('ratio_16_9_small', 360, 200, true);
+        // Ratio 16:9 => Paysage 2
+        add_image_size('ratio_16_9_small', 360, 203, true);
         add_image_size('ratio_16_9_medium', 640, 360, true);
         add_image_size('ratio_16_9', 1200, 675, true);
         add_image_size('ratio_16_9_xlarge', 1920, 1080, true);
 
-        // Ratio 16:10 => Paysage haut
-        add_image_size('ratio_16_10_small', 360, 225, true);
-        add_image_size('ratio_16_10_medium', 640, 400, true);
-        add_image_size('ratio_16_10', 1200, 750, true);
-        add_image_size('ratio_16_10_xlarge', 1920, 1200, true);
 
-        // Square
-        add_image_size('ratio_square_small', 140, 140, true);
-        add_image_size('ratio_square_medium', 360, 360, true);
-        add_image_size('ratio_square', 660, 660, true);
+        // Ratio 4:3 => Paysage 3
+        add_image_size('ratio_4_3_small', 360, 270, true);
+        add_image_size('ratio_4_3_medium', 640, 480, true);
+        add_image_size('ratio_4_3', 1200, 900, true);
+        add_image_size('ratio_4_3_xlarge', 1920, 1440, true);
 
-        // Ratio 10:16 => Portrait large
-        add_image_size('ratio_10_16_small', 200, 320, true);
-        add_image_size('ratio_10_16_medium', 360, 576, true);
-        add_image_size('ratio_10_16', 675, 1080, true);
+        // Carré
+        add_image_size('ratio_square_small', 360, 360, true);
+        add_image_size('ratio_square_medium', 640, 640, true);
+        add_image_size('ratio_square', 1200, 1200, true);
 
-        // Ratio 9:16 => Portrait
-        add_image_size('ratio_9_16_small', 200, 360, true);
-        add_image_size('ratio_9_16_medium', 360, 640, true);
-        add_image_size('ratio_9_16', 675, 1200, true);
+        // Ratio 3:4 => Portrait 1
+        add_image_size('ratio_3_4_small', 360, 480, true);
+        add_image_size('ratio_3_4_medium', 640, 854, true);
+        add_image_size('ratio_3_4', 1200, 1600, true);
+
+        // Ratio 10:16 => Portrait 2
+        add_image_size('ratio_10_16_small', 360, 576, true);
+        add_image_size('ratio_10_16_medium', 640, 1024, true);
+        add_image_size('ratio_10_16', 1200, 1920, true);
+
+        // Ratio A4 => Brochure papier
+        add_image_size('ratio_a4_small', 360, 509, true);
+        add_image_size('ratio_a4_medium', 640, 905, true);
+        add_image_size('ratio_a4', 1200, 1697, true);
 
         // Free => Proportions libre
         add_image_size('ratio_free_small', 360);
@@ -134,61 +140,62 @@ class WoodyTheme_Images
         add_image_size('ratio_free', 1200);
         add_image_size('ratio_free_xlarge', 1920);
 
-        add_filter('image_size_names_choose', array($this, 'basetheme_custom_sizes'));
+        add_filter('image_size_names_choose', array($this, 'woody_custom_sizes'));
         add_filter('wp_calculate_image_sizes', array($this, 'basetheme_adjust_image_sizes_attr'), 10, 2);
         add_filter('post_thumbnail_html', array($this, 'remove_thumbnail_dimensions'), 10, 3);
     }
 
     // Register the new image sizes for use in the add media modal in wp-admin
     // This is the place where you can set readable names for images size
-    public function basetheme_custom_sizes($sizes)
+    public function woody_custom_sizes($sizes)
     {
         return array(
-            'ratio_8_1' => __('Panoramique'),
-            'ratio_4_1' => __('Panoramique moyen'),
-            'ratio_8_3' => __('Paysage long'),
-            'ratio_16_9' => __('Paysage'),
-            'ratio_16_10' => __('Paysage haut'),
+            'ratio_8_1' => __('Panoramique 1 (1200x150px)'),
+            'ratio_4_1' => __('Panoramique 2 (1200x300px)'),
+            'ratio_2_1' => __('Paysage 1 (1200x600px)'),
+            'ratio_16_9' => __('Paysage 2 (1200x675px)'),
+            'ratio_4_3' => __('Paysage 3 (1200x900px)'),
             'ratio_square' => __('Carré'),
-            'ratio_10_16' => __('Portrait large'),
-            'ratio_9_16' => __('Portrait'),
+            'ratio_3_4_medium' => __('Portrait 1 (640x853px)'),
+            'ratio_10_16_medium' => __('Portrait 2 (640x1024px)'),
+            'ratio_a4_medium' => __('Format A4 (640x905px)'),
             'ratio_free' => __('Proportions libres')
         );
     }
 
     // Add custom image sizes attribute to enhance responsive image functionality for content images
-    public function basetheme_adjust_image_sizes_attr($sizes, $size)
-    {
+    // public function basetheme_adjust_image_sizes_attr($sizes, $size)
+    // {
 
-        // Actual width of image
-        $width = $size[0];
+    //     // Actual width of image
+    //     $width = $size[0];
 
-        // Full width page template
-        if (is_page_template('page-templates/page-full-width.php')) {
-            if (1200 < $width) {
-                $sizes = '(max-width: 1199px) 98vw, 1200px';
-            } else {
-                $sizes = '(max-width: 1199px) 98vw, ' . $width . 'px';
-            }
-        } else { // Default 3/4 column post/page layout
-            if (770 < $width) {
-                $sizes = '(max-width: 639px) 98vw, (max-width: 1199px) 64vw, 770px';
-            } else {
-                $sizes = '(max-width: 639px) 98vw, (max-width: 1199px) 64vw, ' . $width . 'px';
-            }
-        }
+    //     // Full width page template
+    //     if (is_page_template('page-templates/page-full-width.php')) {
+    //         if (1200 < $width) {
+    //             $sizes = '(max-width: 1199px) 98vw, 1200px';
+    //         } else {
+    //             $sizes = '(max-width: 1199px) 98vw, ' . $width . 'px';
+    //         }
+    //     } else { // Default 3/4 column post/page layout
+    //         if (770 < $width) {
+    //             $sizes = '(max-width: 639px) 98vw, (max-width: 1199px) 64vw, 770px';
+    //         } else {
+    //             $sizes = '(max-width: 639px) 98vw, (max-width: 1199px) 64vw, ' . $width . 'px';
+    //         }
+    //     }
 
-        return $sizes;
-    }
+    //     return $sizes;
+    // }
 
     // Remove inline width and height attributes for post thumbnails
-    public function remove_thumbnail_dimensions($html, $post_id, $post_image_id)
-    {
-        if (!strpos($html, 'attachment-shop_single')) {
-            $html = preg_replace('/^(width|height)=\"\d*\"\s/', '', $html);
-        }
-        return $html;
-    }
+    // public function remove_thumbnail_dimensions($html, $post_id, $post_image_id)
+    // {
+    //     if (!strpos($html, 'attachment-shop_single')) {
+    //         $html = preg_replace('/^(width|height)=\"\d*\"\s/', '', $html);
+    //     }
+    //     return $html;
+    // }
 }
 
 // Execute Class
