0f889fe66384a1c9010e9c0fc94d9db16e0504e4
Léo POIROUX <leo.poiroux@gmail.com>
2018-07-23 09:51:01 +0200

    Add Alt

--- library/content/class-images.php
+++ library/content/class-images.php
@@ -15,74 +15,8 @@ class WoodyTheme_Images
         $this->register_hooks();
     }
 
-    // public static function imagemagick(WP_REST_Request $request)
-    // {
-    //     /**
-    //      * Exemple : http://www.superot.wp.rc-dev.com/wp-json/woody/crop/50/100/aHR0cDovL3d3dy5zdXBlcm90LndwLnJjLWRldi5jb20vYXBwL3VwbG9hZHMvc3VwZXJvdC8yMDE4LzA3L3Blb3BsZS1tYW4tMi5qcGc=
-    //      */
-
-    //     // Get parameters
-    //     $params = $request->get_params();
-    //     $width = $params['width'];
-    //     $height = $params['height'];
-
-    //     // Filename
-    //     $url = parse_url(base64_decode($params['url']));
-    //     $filename = WP_WEBROOT_DIR . $url['path'];
-    //     if (!file_exists($filename)) {
-    //         die('Image introuvale');
-    //     }
-
-    //     // New Filename
-    //     $ext = pathinfo($filename, PATHINFO_EXTENSION);
-    //     $new_filename = str_replace('.'.$ext, '-'.$width.'x'.$height.'.'.$ext, $filename);
-
-    //     // Imagick execution
-    //     if (!file_exists($new_filename)) {
-    //         $image = new Imagick($filename);
-    //         $w = $image->getImageWidth();
-    //         $h = $image->getImageHeight();
-
-    //         if ($w == $h) {
-    //             if ($width >= $height) {
-    //                 $resize_w = $width;
-    //                 $resize_h = $width;
-    //             } else {
-    //                 $resize_w = $height;
-    //                 $resize_h = $height;
-    //             }
-    //         } elseif ($w > $h) {
-    //             $resize_w = $w * $height / $h;
-    //             $resize_h = $height;
-    //         } else {
-    //             $resize_w = $width;
-    //             $resize_h = $h * $width / $w;
-    //         }
-
-    //         $image->resizeImage($resize_w, $resize_h, Imagick::FILTER_LANCZOS, 0.9);
-    //         $image->cropImage($width, $height, ($resize_w - $width) / 2, ($resize_h - $height) / 2);
-    //         $image->writeImage($new_filename);
-    //     }
-
-    //     if (file_exists($new_filename)) {
-    //         header('Content-type: ' . mime_content_type($new_filename));
-    //         header('Cache-Control: max-age=315360000');
-    //         header('Expires: Thu, 31 Dec 2037 23:55:55 GMT');
-    //         print file_get_contents($new_filename);
-    //     } else {
-    //         die('Erreur de génération de la miniature');
-    //     }
-    // }
-
     protected function register_hooks()
     {
-        // add_action('rest_api_init', function () {
-        //     register_rest_route('woody', '/crop/(?P<width>[0-9]{1,4})/(?P<height>[0-9]{1,4})/(?P<url>[-=\w]+)', array(
-        //       'methods' => 'GET',
-        //       'callback' => array('WoodyTheme_Images', 'imagemagick')
-        //     ));
-        // });
-
         // Ratio 8:1 => Panoramique 1
         add_image_size('ratio_8_1_small', 360, 45, true);
         add_image_size('ratio_8_1_medium', 640, 80, true);
@@ -143,6 +77,15 @@ class WoodyTheme_Images
         add_filter('image_size_names_choose', array($this, 'woody_custom_sizes'));
         add_filter('wp_calculate_image_sizes', array($this, 'basetheme_adjust_image_sizes_attr'), 10, 2);
         add_filter('post_thumbnail_html', array($this, 'remove_thumbnail_dimensions'), 10, 3);
+
+        // add_action('add_attachment', array($this, 'set_image_meta_upon_image_upload'));
+
+        // add_action('rest_api_init', function () {
+        //     register_rest_route('woody', '/crop/(?P<width>[0-9]{1,4})/(?P<height>[0-9]{1,4})/(?P<url>[-=\w]+)', array(
+        //       'methods' => 'GET',
+        //       'callback' => array('WoodyTheme_Images', 'imagemagick')
+        //     ));
+        // });
     }
 
     // Register the new image sizes for use in the add media modal in wp-admin
@@ -163,38 +106,93 @@ class WoodyTheme_Images
         );
     }
 
-    // Add custom image sizes attribute to enhance responsive image functionality for content images
-    // public function basetheme_adjust_image_sizes_attr($sizes, $size)
+    // public function set_image_meta_upon_image_upload($post_ID)
     // {
+    //     // Automatically set the image Title, Alt-Text, Caption & Description upon upload
+    //     // Check if uploaded file is an image, else do nothing
+    //     if (wp_attachment_is_image($post_ID)) {
+    //         $my_image_title = get_post($post_ID)->post_title;
 
-    //     // Actual width of image
-    //     $width = $size[0];
+    //         // Sanitize the title:  remove hyphens, underscores & extra spaces:
+    //         $my_image_title = preg_replace('%\s*[-_\s]+\s*%', ' ', $my_image_title);
 
-    //     // Full width page template
-    //     if (is_page_template('page-templates/page-full-width.php')) {
-    //         if (1200 < $width) {
-    //             $sizes = '(max-width: 1199px) 98vw, 1200px';
-    //         } else {
-    //             $sizes = '(max-width: 1199px) 98vw, ' . $width . 'px';
-    //         }
-    //     } else { // Default 3/4 column post/page layout
-    //         if (770 < $width) {
-    //             $sizes = '(max-width: 639px) 98vw, (max-width: 1199px) 64vw, 770px';
-    //         } else {
-    //             $sizes = '(max-width: 639px) 98vw, (max-width: 1199px) 64vw, ' . $width . 'px';
-    //         }
+    //         // Sanitize the title:  capitalize first letter of every word (other letters lower case):
+    //         $my_image_title = ucwords(strtolower($my_image_title));
+
+    //         // Create an array with the image meta (Title, Caption, Description) to be updated
+    //         // Note:  comment out the Excerpt/Caption or Content/Description lines if not needed
+    //         $my_image_meta = array(
+    //             'ID'		=> $post_ID,			// Specify the image (ID) to be updated
+    //             'post_title'	=> $my_image_title,		// Set image Title to sanitized title
+    //             'post_excerpt'	=> $my_image_title,		// Set image Caption (Excerpt) to sanitized title
+    //             'post_content'	=> $my_image_title,		// Set image Description (Content) to sanitized title
+    //         );
+
+    //         // Set the image Alt-Text
+    //         update_post_meta($post_ID, '_wp_attachment_image_alt', $my_image_title);
+
+    //         // Set the image meta (e.g. Title, Excerpt, Content)
+    //         wp_update_post($my_image_meta);
     //     }
-
-    //     return $sizes;
     // }
 
-    // Remove inline width and height attributes for post thumbnails
-    // public function remove_thumbnail_dimensions($html, $post_id, $post_image_id)
+    // public static function imagemagick(WP_REST_Request $request)
     // {
-    //     if (!strpos($html, 'attachment-shop_single')) {
-    //         $html = preg_replace('/^(width|height)=\"\d*\"\s/', '', $html);
+    //     /**
+    //      * Exemple : http://www.superot.wp.rc-dev.com/wp-json/woody/crop/50/100/aHR0cDovL3d3dy5zdXBlcm90LndwLnJjLWRldi5jb20vYXBwL3VwbG9hZHMvc3VwZXJvdC8yMDE4LzA3L3Blb3BsZS1tYW4tMi5qcGc=
+    //      */
+
+    //     // Get parameters
+    //     $params = $request->get_params();
+    //     $width = $params['width'];
+    //     $height = $params['height'];
+
+    //     // Filename
+    //     $url = parse_url(base64_decode($params['url']));
+    //     $filename = WP_WEBROOT_DIR . $url['path'];
+    //     if (!file_exists($filename)) {
+    //         die('Image introuvale');
     //     }
-    //     return $html;
+
+    //     // New Filename
+    //     $ext = pathinfo($filename, PATHINFO_EXTENSION);
+    //     $new_filename = str_replace('.'.$ext, '-'.$width.'x'.$height.'.'.$ext, $filename);
+
+    //     // Imagick execution
+    //     if (!file_exists($new_filename)) {
+    //         $image = new Imagick($filename);
+    //         $w = $image->getImageWidth();
+    //         $h = $image->getImageHeight();
+
+    //         if ($w == $h) {
+    //             if ($width >= $height) {
+    //                 $resize_w = $width;
+    //                 $resize_h = $width;
+    //             } else {
+    //                 $resize_w = $height;
+    //                 $resize_h = $height;
+    //             }
+    //         } elseif ($w > $h) {
+    //             $resize_w = $w * $height / $h;
+    //             $resize_h = $height;
+    //         } else {
+    //             $resize_w = $width;
+    //             $resize_h = $h * $width / $w;
+    //         }
+
+    //         $image->resizeImage($resize_w, $resize_h, Imagick::FILTER_LANCZOS, 0.9);
+    //         $image->cropImage($width, $height, ($resize_w - $width) / 2, ($resize_h - $height) / 2);
+    //         $image->writeImage($new_filename);
+    //     }
+
+    //     if (file_exists($new_filename)) {
+    //         header('Content-type: ' . mime_content_type($new_filename));
+    //         header('Cache-Control: max-age=315360000');
+    //         header('Expires: Thu, 31 Dec 2037 23:55:55 GMT');
+    //         print file_get_contents($new_filename);
+    //     } else {
+    //         die('Erreur de génération de la miniature');
+    //     }
     // }
 }
 
