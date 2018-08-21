fb934caa46715508b5713d56b480c8c5c8d2930f
Léo POIROUX <leo.poiroux@gmail.com>
2018-08-21 17:14:05 +0200

    dashboard

--- library/classes/cleanup/class-admin.php
+++ library/classes/cleanup/class-admin.php
@@ -165,8 +165,23 @@ class WoodyTheme_Cleanup_Admin
      */
     public function removeDashboardWidgets()
     {
+        // global $wp_meta_boxes;
+        // \PC::debug($wp_meta_boxes);
+        remove_meta_box('dashboard_right_now', 'dashboard', 'normal');
         remove_meta_box('dashboard_activity', 'dashboard', 'normal');
         remove_meta_box('dashboard_primary', 'dashboard', 'side');
+        remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
+
+        wp_add_dashboard_widget(
+            'raccourci-family', // Widget slug.
+            'Raccourci Family', // Title.
+            [$this, 'raccourciFamilyWidget'] // Display function.
+        );
+    }
+
+    public function raccourciFamilyWidget()
+    {
+        echo '<table border="0" width="100%" cellspacing="0" cellpadding="0" style="border-collapse:collapse;"><tr style=""><td height="28" style="line-height:28px;">&nbsp;</td></tr><tr><td style=""><table border="0" width="100%" cellspacing="0" cellpadding="0" style="border-collapse:separate;background-color:#ffffff;font-family:Helvetica, Arial, sans-serif;margin:0px auto;"><tr style="padding-bottom: 8px;"><td style=""><img class="img" src="https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-0/c0.0.567.296/p526x296/36305656_10215635967506764_1276999918627586048_o.jpg?_nc_cat=0&amp;oh=85ecdbd5225d67c913341c8ae3b21aae&amp;oe=5BF38F19" width="280" height="146" alt="" /></td></tr><tr><td style="font-size:14px;font-weight:bold;padding:8px 8px 0px 8px;text-align:center;">Raccourci Family</td></tr><tr><td style="color:#90949c;font-size:12px;font-weight:normal;text-align:center;">Groupe Facebook · 287 membres</td></tr><tr><td style="padding:8px 12px 12px 12px;"><table border="0" cellspacing="0" cellpadding="0" style="border-collapse:collapse;width:100%;"><tr><td style="background-color:#4267b2;border-radius:3px;text-align:center;"><a style="color:#3b5998;text-decoration:none;cursor:pointer;width:100%;" href="https://www.facebook.com/plugins/group/join/popup/?group_id=355097798174987&amp;source=email_campaign_plugin" target="_blank" rel="noopener"><table border="0" cellspacing="0" cellpadding="3" align="center" style="border-collapse:collapse;"><tr><td style="border-bottom:3px solid #4267b2;border-top:3px solid #4267b2;"><img width="16" src="https://facebook.com/images/groups/plugin/email/app_fb_32_fig_white.png" /></td><td style="border-bottom:3px solid #4267b2;border-top:3px solid #4267b2;color:#FFF;font-family:Helvetica, Arial, sans-serif;font-size:12px;font-weight:bold;">Rejoindre ce groupe</td></tr></table></a></td></tr></table></td></tr><tr><td style="border-top:1px solid #dddfe2;font-size:12px;padding:8px 12px;">Un lieu d&#039;échange de partage et de bonnes idées entre tous les membres de la Raccourci Family !</td></tr></table></td></tr><tr style=""><td height="14" style="line-height:14px;">&nbsp;</td></tr></table>';
     }
 
     /**
