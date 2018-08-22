045460f0de4b0dbf8d06fa1afc4d7e8322d5c17d
Léo POIROUX <leo.poiroux@gmail.com>
2018-08-22 14:22:05 +0200

    selectize

--- src/js/admin/selectize.js
+++ src/js/admin/selectize.js
@@ -3,6 +3,7 @@ import * as selectize from "selectize";
 import * as arrive from "arrive";
 
 var attachment_selectize = function(target) {
+    console.log('attachment OK');
     $(target)
         .css('overflow', 'unset')
         .find('input:not(.selectized)').selectize({
@@ -29,15 +30,11 @@ var waiting = function(container, target, callback) {
 }
 
 waiting('#wp-media-grid', '.attachment.save-ready', function() {
-    $('.attachment.save-ready').click(function() {
-        //console.log('click save');
-        waiting('.media-sidebar', '.compat-field-attachment_hashtags', attachment_selectize('.compat-field-attachment_hashtags'));
-
+    $('#wp-media-grid .attachment.save-ready').click(function() {
+        attachment_selectize('.media-sidebar .compat-field-attachment_hashtags')
         $('.edit-attachment').click(function() {
-            //console.log('click edit');
-            waiting(document, '.media-modal-content', function() {
-                //console.log('modal-load');
-                waiting('.media-modal-content', '.compat-field-attachment_hashtags', attachment_selectize('.compat-field-attachment_hashtags'));
+            waiting('body', '.media-modal-content', function() {
+                attachment_selectize('.media-modal-content .compat-field-attachment_hashtags');
             });
         });
     });
