e7321733d1a66ec77125271b3cd30118c94b06a2
Léo POIROUX <leo.poiroux@gmail.com>
2018-08-30 19:03:04 +0200

    bugfix

--- library/classes/content/class-taxonomy.php
+++ library/classes/content/class-taxonomy.php
@@ -16,7 +16,7 @@ class WoodyTheme_Taxonomy
 
     protected function registerHooks()
     {
-        add_action('woody_update', array($this, 'registerContentTypeTaxonomy'), 10);
+        add_action('admin_init', array($this, 'registerContentTypeTaxonomy'), 10);
     }
 
     public function registerContentTypeTaxonomy()
@@ -64,97 +64,96 @@ class WoodyTheme_Taxonomy
         wp_insert_term('Brochure', 'page_type', array('slug' => 'booklet'));
         wp_insert_term('Page d\'accueil', 'page_type', array('slug' => 'front_page'));
 
-
         // On créé la taxonomie "Thématiques"
         register_taxonomy(
-            'themes',
-            ['page', 'attachment'],
-            array(
-                'label' => 'Thématiques',
-                'labels' => [
-                    'name' => 'Thématiques',
-                    'singular_name' => 'Thématique',
-                    'menu_name' => 'Thématiques',
-                    'all_items' => 'Toutes les thématiques',
-                    'edit_item' => 'Modifier les thématiques',
-                    'view_item' => 'Voir les thématiques',
-                    'update_item' => 'Mettre à jour les thématiques',
-                    'add_new_item' => 'Ajouter une thématique',
-                    'new_item_name' => 'Nouvelle thématique',
-                    'search_items' => 'Rechercher parmi les thématiques',
-                    'popular_items' => 'Thématiques les plus utilisées'
-                ],
-                'hierarchical' => true,
-                'show_ui' => true,
-                'capabilities' => [
-                    'manage_terms' => 'Configurer les thématiques',
-                    'edit_terms' => 'Editer les thématiques',
-                    'delete_terms' => 'Supprimer les thématiques',
-                    'assign_terms' => 'Assigner les thématiques'
-                ]
-            )
-        );
+                'themes',
+                ['page', 'attachment'],
+                array(
+                    'label' => 'Thématiques',
+                    'labels' => [
+                        'name' => 'Thématiques',
+                        'singular_name' => 'Thématique',
+                        'menu_name' => 'Thématiques',
+                        'all_items' => 'Toutes les thématiques',
+                        'edit_item' => 'Modifier les thématiques',
+                        'view_item' => 'Voir les thématiques',
+                        'update_item' => 'Mettre à jour les thématiques',
+                        'add_new_item' => 'Ajouter une thématique',
+                        'new_item_name' => 'Nouvelle thématique',
+                        'search_items' => 'Rechercher parmi les thématiques',
+                        'popular_items' => 'Thématiques les plus utilisées'
+                    ],
+                    'hierarchical' => true,
+                    'show_ui' => true,
+                    'capabilities' => [
+                        'manage_terms' => 'Configurer les thématiques',
+                        'edit_terms' => 'Editer les thématiques',
+                        'delete_terms' => 'Supprimer les thématiques',
+                        'assign_terms' => 'Assigner les thématiques'
+                    ]
+                )
+            );
 
         // On créé la taxonomie "Lieux"
         register_taxonomy(
-            'places',
-            ['page', 'attachment'],
-            array(
-                'label' => 'Lieux',
-                'labels' => [
-                    'name' => 'Lieux',
-                    'singular_name' => 'Lieu',
-                    'menu_name' => 'Lieux',
-                    'all_items' => 'Tous les lieux',
-                    'edit_item' => 'Modifier les lieux',
-                    'view_item' => 'Voir les lieux',
-                    'update_item' => 'Mettre à jour les lieux',
-                    'add_new_item' => 'Ajouter un lieu',
-                    'new_item_name' => 'Nouveau lieu',
-                    'search_items' => 'Rechercher parmi les lieux',
-                    'popular_items' => 'Lieux les plus utilisés'
-                ],
-                'hierarchical' => true,
-                'show_ui' => true,
-                'capabilities' => [
-                    'manage_terms' => 'Configurer les lieux',
-                    'edit_terms' => 'Editer les lieux',
-                    'delete_terms' => 'Supprimer les lieux',
-                    'assign_terms' => 'Assigner les lieux'
-                ]
-            )
-        );
+                'places',
+                ['page', 'attachment'],
+                array(
+                    'label' => 'Lieux',
+                    'labels' => [
+                        'name' => 'Lieux',
+                        'singular_name' => 'Lieu',
+                        'menu_name' => 'Lieux',
+                        'all_items' => 'Tous les lieux',
+                        'edit_item' => 'Modifier les lieux',
+                        'view_item' => 'Voir les lieux',
+                        'update_item' => 'Mettre à jour les lieux',
+                        'add_new_item' => 'Ajouter un lieu',
+                        'new_item_name' => 'Nouveau lieu',
+                        'search_items' => 'Rechercher parmi les lieux',
+                        'popular_items' => 'Lieux les plus utilisés'
+                    ],
+                    'hierarchical' => true,
+                    'show_ui' => true,
+                    'capabilities' => [
+                        'manage_terms' => 'Configurer les lieux',
+                        'edit_terms' => 'Editer les lieux',
+                        'delete_terms' => 'Supprimer les lieux',
+                        'assign_terms' => 'Assigner les lieux'
+                    ]
+                )
+            );
 
         // On créé la taxonomie "Saisons"
         register_taxonomy(
-            'seasons',
-            ['page', 'attachment'],
-            array(
-                'label' => 'Saisons',
-                'labels' => [
-                    'name' => 'Saisons',
-                    'singular_name' => 'Saison',
-                    'menu_name' => 'Saisons',
-                    'all_items' => 'Toutes les saisons',
-                    'edit_item' => 'Modifier les saisons',
-                    'view_item' => 'Voir les saisons',
-                    'update_item' => 'Mettre à jour les saisons',
-                    'add_new_item' => 'Ajouter une saison',
-                    'new_item_name' => 'Nouvelle saison',
-                    'search_items' => 'Rechercher parmi les saisons',
-                    'popular_items' => 'Saisons les plus utilisées'
-                ],
-                'hierarchical' => false,
-                'show_ui' => true,
-                'show_in_menu' => true,
-                'capabilities' => [
-                    'manage_terms' => 'Configurer les saisons',
-                    'edit_terms' => 'Editer les saisons',
-                    'delete_terms' => 'Supprimer les saison',
-                    'assign_terms' => 'Assigner les saisons'
-                ]
-            )
-        );
+                'seasons',
+                ['page', 'attachment'],
+                array(
+                    'label' => 'Saisons',
+                    'labels' => [
+                        'name' => 'Saisons',
+                        'singular_name' => 'Saison',
+                        'menu_name' => 'Saisons',
+                        'all_items' => 'Toutes les saisons',
+                        'edit_item' => 'Modifier les saisons',
+                        'view_item' => 'Voir les saisons',
+                        'update_item' => 'Mettre à jour les saisons',
+                        'add_new_item' => 'Ajouter une saison',
+                        'new_item_name' => 'Nouvelle saison',
+                        'search_items' => 'Rechercher parmi les saisons',
+                        'popular_items' => 'Saisons les plus utilisées'
+                    ],
+                    'hierarchical' => false,
+                    'show_ui' => true,
+                    'show_in_menu' => true,
+                    'capabilities' => [
+                        'manage_terms' => 'Configurer les saisons',
+                        'edit_terms' => 'Editer les saisons',
+                        'delete_terms' => 'Supprimer les saison',
+                        'assign_terms' => 'Assigner les saisons'
+                    ]
+                )
+            );
 
         // On inclut les termes génériques à la taxo
         wp_insert_term('Été', 'seasons', array('slug' => 'summer'));
@@ -164,63 +163,63 @@ class WoodyTheme_Taxonomy
 
         // On créé la taxonomie "Types de média"
         register_taxonomy(
-            'attachment_types',
-            'attachment',
-            array(
-                'label' => 'Types de média',
-                'labels' => [
-                    'name' => 'Types de média',
-                    'singular_name' => 'Type de média',
-                    'menu_name' => 'Types de média',
-                    'all_items' => 'Toutes les types de média',
-                    'edit_item' => 'Modifier les types de média',
-                    'view_item' => 'Voir les types de média',
-                    'update_item' => 'Mettre à jour les types de média',
-                    'add_new_item' => 'Ajouter une type de média',
-                    'new_item_name' => 'Nouveau type de média',
-                    'search_items' => 'Rechercher parmi les types de média',
-                    'popular_items' => 'Types de média les plus utilisés'
-                ],
-                'hierarchical' => false,
-                'show_ui' => false,
-                'capabilities' => [
-                    'manage_terms' => 'Configurer les types de médias',
-                    'edit_terms' => 'Editer les types de médias',
-                    'delete_terms' => 'Supprimer les types de médias',
-                    'assign_terms' => 'Assigner les types de médias'
-                ]
-            )
-        );
+                'attachment_types',
+                'attachment',
+                array(
+                    'label' => 'Types de média',
+                    'labels' => [
+                        'name' => 'Types de média',
+                        'singular_name' => 'Type de média',
+                        'menu_name' => 'Types de média',
+                        'all_items' => 'Toutes les types de média',
+                        'edit_item' => 'Modifier les types de média',
+                        'view_item' => 'Voir les types de média',
+                        'update_item' => 'Mettre à jour les types de média',
+                        'add_new_item' => 'Ajouter une type de média',
+                        'new_item_name' => 'Nouveau type de média',
+                        'search_items' => 'Rechercher parmi les types de média',
+                        'popular_items' => 'Types de média les plus utilisés'
+                    ],
+                    'hierarchical' => false,
+                    'show_ui' => false,
+                    'capabilities' => [
+                        'manage_terms' => 'Configurer les types de médias',
+                        'edit_terms' => 'Editer les types de médias',
+                        'delete_terms' => 'Supprimer les types de médias',
+                        'assign_terms' => 'Assigner les types de médias'
+                    ]
+                )
+            );
 
         // On créé la taxonomie "Catégories de média"
         register_taxonomy(
-            'attachment_categories',
-            'attachment',
-            array(
-                'label' => 'Catégories de média',
-                'labels' => [
-                    'name' => 'Catégories de média',
-                    'singular_name' => 'Catégorie du média',
-                    'menu_name' => 'Catégories de média',
-                    'all_items' => 'Toutes les catégories de média',
-                    'edit_item' => 'Modifier les catégories de média',
-                    'view_item' => 'Voir les catégories de média',
-                    'update_item' => 'Mettre à jour les catégories de média',
-                    'add_new_item' => 'Ajouter une catégorie de média',
-                    'new_item_name' => 'Nouvelle catégorie de média',
-                    'search_items' => 'Rechercher parmi les catégories de média',
-                    'popular_items' => 'Catégories de média les plus utilisés'
-                ],
-                'hierarchical' => true,
-                'show_ui' => true,
-                'capabilities' => [
-                    'manage_terms' => 'Configurer les catégories de médias',
-                    'edit_terms' => 'Editer les catégories de médias',
-                    'delete_terms' => 'Supprimer les catégories de médias',
-                    'assign_terms' => 'Assigner les catégories de médias'
-                ]
-            )
-        );
+                'attachment_categories',
+                'attachment',
+                array(
+                    'label' => 'Catégories de média',
+                    'labels' => [
+                        'name' => 'Catégories de média',
+                        'singular_name' => 'Catégorie du média',
+                        'menu_name' => 'Catégories de média',
+                        'all_items' => 'Toutes les catégories de média',
+                        'edit_item' => 'Modifier les catégories de média',
+                        'view_item' => 'Voir les catégories de média',
+                        'update_item' => 'Mettre à jour les catégories de média',
+                        'add_new_item' => 'Ajouter une catégorie de média',
+                        'new_item_name' => 'Nouvelle catégorie de média',
+                        'search_items' => 'Rechercher parmi les catégories de média',
+                        'popular_items' => 'Catégories de média les plus utilisés'
+                    ],
+                    'hierarchical' => true,
+                    'show_ui' => true,
+                    'capabilities' => [
+                        'manage_terms' => 'Configurer les catégories de médias',
+                        'edit_terms' => 'Editer les catégories de médias',
+                        'delete_terms' => 'Supprimer les catégories de médias',
+                        'assign_terms' => 'Assigner les catégories de médias'
+                    ]
+                )
+            );
 
         // On créé la taxonomie "Hashtags"
         register_taxonomy(
@@ -243,7 +242,7 @@ class WoodyTheme_Taxonomy
                 'hierarchical' => false,
                 'show_ui' => true,
                 'capabilities' => [
-                     'manage_terms' => 'Configurer les hashtag',
+                    'manage_terms' => 'Configurer les hashtag',
                     'edit_terms' => 'Editer les hashtag',
                     'delete_terms' => 'Supprimer les hashtag',
                     'assign_terms' => 'Assigner les hashtag'
