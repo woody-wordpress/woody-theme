bf77173975aa21469c191260bdfe01d3cded3139
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2018-09-11 16:44:27 +0200

    Added buttons in tinymce

---
+++ library/classes/content/class-tinymce.php
@@ -0,0 +1,45 @@
+<?php
+class WoodyTheme_Tinymce
+{
+    public function __construct()
+    {
+        $this->registerHooks();
+    }
+
+    protected function registerHooks()
+    {
+        add_filter('mce_buttons_2', array($this, 'tinymceAddStyleSelect'));
+        add_filter('tiny_mce_before_init', array($this, 'tinymceRegisterStyleSelect'));
+        // add_action('init', array($this, 'tinymceAddStylesheet'));
+    }
+
+    // Callback function to insert 'styleselect' into the $buttons array
+    public function tinymceAddStyleSelect($buttons)
+    {
+        array_unshift($buttons, 'styleselect');
+        return $buttons;
+    }
+
+    public function tinymceRegisterStyleSelect($init_array)
+    {
+        $style_formats = array(
+        array(
+            'title' => 'Bouton 1',
+            'selector' => 'a',
+            'classes' => 'button primary'
+        ),
+        array(
+            'title' => 'Bouton 2',
+            'selector' => 'a',
+            'classes' => 'button secondary'
+        ),
+    );
+        $init_array['style_formats'] = json_encode($style_formats);
+        return $init_array;
+    }
+
+    // public function tinymceAddStylesheet()
+    // {
+    //     add_editor_style('custom-editor-style.css');
+    // }
+}
