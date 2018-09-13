cf5d3dd4bfac160b4745653855e9e7866c54fcbf
Léo POIROUX <leo.poiroux@gmail.com>
2018-09-13 11:00:12 +0200

    Improve save post + remove jquery migrate en preprod/prod

--- src/js/admin/save-post.js
+++ src/js/admin/save-post.js
@@ -1,29 +1,42 @@
 import $ from 'jquery';
 
-$('body.post-type-page form#post').each(function() {
-    var $button = $('input#publish[value="Mettre à jour"]');
-    $button.addClass('woody-update');
+$('body.post-type-page.post-php form#post').submit(function(e) {
+    e.preventDefault();
 
-    $button.parents('form').submit(function(e) {
-        var $spinner = $button.siblings('.spinner');
+    // Change Action
+    $('#hiddenaction').attr('value', 'editajaxpost');
 
-        $button.addClass('disabled');
-        $spinner.addClass('is-active');
-        $('#hiddenaction').attr('value', 'editajaxpost');
+    var $button_publish = $('#publish');
+    var $button_preview = $('#post-preview');
+    var $spinner = $button_publish.siblings('.spinner');
 
-        $.ajax({
-            type: "POST",
-            data: $(this).serialize(),
-            url: "post.php",
-            success: function(data) {
-                $button.removeClass('disabled');
-                $spinner.removeClass('is-active');
-            },
-            error: function() {
-                alert('error')
-            }
-        });
-
-        e.preventDefault();
+    $button_preview.click(function() {
+        $(this).addClass('clickAfterAjax');
     });
+
+    $button_publish.addClass('disabled');
+    $button_preview.addClass('disabled');
+    $spinner.addClass('is-active');
+
+    $.ajax({
+        type: "POST",
+        data: $(this).serialize(),
+        url: "post.php",
+        success: function(data) {
+            $button_publish.removeClass('disabled');
+            $button_preview.removeClass('disabled');
+            $spinner.removeClass('is-active');
+
+            // If preview button click, open new brower tab
+            if ($button_preview.hasClass('clickAfterAjax')) {
+                $button_preview.removeClass('clickAfterAjax');
+                window.location.replace($button_preview.attr('href'));
+            }
+        },
+        error: function() {
+            console.log('error');
+        }
+    });
+
+    return false;
 });
