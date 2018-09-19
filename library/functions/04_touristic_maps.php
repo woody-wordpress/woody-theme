d7e5e93d42a4999c6e57a1deaedfe8c3dcc101c6
Rudy Danesi <rudy.danesi@raccourci.fr>
2018-09-19 15:12:28 +0200

    touristic map OK

--- library/functions/04_touristic_maps.php
+++ library/functions/04_touristic_maps.php
@@ -14,14 +14,14 @@ function getMapKeys($encoded = false)
     $gmapkeys = RC_GOOGLE_MAPS_API_KEY;
     $ignkeys = RC_IGN_MAPS_API_KEY;
 
-    if (is_array($otmkeys) && !empty($otmkeys)) {
-        $map_keys['otmKey'] = (WP_ENV === 'dev') ? 'raccourci' : shuffle($otmkeys)[0]; // override touristic_map key in DEV
+    if (is_array($otmkeys) && !empty($otmkeys) && !empty($otmkeys[0])) {
+        shuffle($otmkeys) && $map_keys['otmKey'] = (WP_ENV === 'dev') ? 'raccourci' : $otmkeys[0]; // override touristic_map key in DEV
     }
-    if (is_array($gmapkeys) && !empty($gmapkeys)) {
-        $map_keys['gmKey'] = shuffle($gmapkeys)[0];
+    if (is_array($gmapkeys) && !empty($gmapkeys) && !empty($gmapkeys[0])) {
+        shuffle($gmapkeys) && $map_keys['gmKey'] =  $gmapkeys[0];
     }
-    if (is_array($ignkeys) && !empty($ignkeys)) {
-        $map_keys['ignKey'] = shuffle($ignkeys)[0];
+    if (is_array($ignkeys) && !empty($ignkeys) && !empty($ignkeys[0])) {
+        shuffle($ignkeys) && $map_keys['ignKey'] =  $ignkeys[0];
     }
 
     return $encoded ? wp_json_encode($map_keys) : $map_keys;
