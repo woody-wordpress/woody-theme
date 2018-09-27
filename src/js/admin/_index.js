5208f513181e4ffe3fa9db2ad735c8a183addd8d
Léo POIROUX <leo.poiroux@gmail.com>
2018-09-27 09:07:26 +0200

    move hawwwai button to woody plugin

--- src/js/admin/_index.js
+++ src/js/admin/_index.js
@@ -1,8 +1,12 @@
+// Admin
 import './page-loader';
 import './edit-post';
 import './nav-menus-ui';
 import './global-admin';
 
-//import './woodyPlugin-popup';
+// Woody Plugin
+import 'Hawwwai/Resources/Assets/js/acf-hawwwai'
+
+// Disabled
 //import './save-post';
 //import './selectize';
