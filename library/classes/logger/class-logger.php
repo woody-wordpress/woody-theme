b35f6e65af805530bfc8cff19aa1be7599a9f7e8
Rudy Danesi <rudy.danesi@raccourci.fr>
2018-09-27 16:48:24 +0200

    update logger

--- library/classes/logger/class-logger.php
+++ library/classes/logger/class-logger.php
@@ -2,22 +2,26 @@
 
 class WoodyTheme_Logger
 {
+    // main files
+    const DEBUG = 'debug';
+    const WARMUP = 'last_warmup';
+
     /**
      * Log
      *
      * @param string $message
      */
-    public function log($message, $file = 'debug')
+    public static function log($message, $file = self::DEBUG, $rewrite = false)
     {
         $file = WP_ROOT_DIR . '/logs/' . $file . '.log';
 
-        if (file_exists($file)) {
+        if (file_exists($file) && !$rewrite) {
             $existing_log = file_get_contents($file);
         } else {
             $existing_log = '';
         }
 
-        if (is_array($message)) {
+        if (is_array($message) || is_object($message)) {
             $message = json_encode($message, true);
         }
 
