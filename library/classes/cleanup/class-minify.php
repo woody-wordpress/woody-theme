dd338a1296971ac149f6676a3974c92800413add
Rudy Danesi <rudy.danesi@raccourci.fr>
2018-10-09 12:23:30 +0200

    update minify

--- library/classes/cleanup/class-minify.php
+++ library/classes/cleanup/class-minify.php
@@ -6,6 +6,8 @@
  * @since WoodyTheme 1.0.0
  */
 
+use voku\helper\HtmlMin;
+
 class WoodyTheme_Cleanup_Minify
 {
     public function __construct()
@@ -28,78 +30,30 @@ class WoodyTheme_Cleanup_Minify
 
     public function minifyHtmlOutput($buffer)
     {
-        if (substr(ltrim($buffer), 0, 5) == '<?xml') {
-            return ($buffer);
-        }
-        $minify_javascript = get_option('minify_javascript');
-        $minify_html_comments = get_option('minify_html_comments');
-        $minify_html_utf8 = get_option('minify_html_utf8');
-        if ($minify_html_utf8 == 'yes' && mb_detect_encoding($buffer, 'UTF-8', true)) {
-            $mod = '/u';
-        } else {
-            $mod = '/s';
-        }
-        $buffer = str_replace(array(chr(13) . chr(10), chr(9)), array(chr(10), ''), $buffer);
-        $buffer = str_ireplace(array('<script', '/script>', '<pre', '/pre>', '<textarea', '/textarea>', '<style', '/style>'), array('M1N1FY-ST4RT<script', '/script>M1N1FY-3ND', 'M1N1FY-ST4RT<pre', '/pre>M1N1FY-3ND', 'M1N1FY-ST4RT<textarea', '/textarea>M1N1FY-3ND', 'M1N1FY-ST4RT<style', '/style>M1N1FY-3ND'), $buffer);
-        $split = explode('M1N1FY-3ND', $buffer);
-        $buffer = '';
-        for ($i=0; $i<count($split); $i++) {
-            $ii = strpos($split[$i], 'M1N1FY-ST4RT');
-            if ($ii !== false) {
-                $process = substr($split[$i], 0, $ii);
-                $asis = substr($split[$i], $ii + 12);
-                if (substr($asis, 0, 7) == '<script') {
-                    $split2 = explode(chr(10), $asis);
-                    $asis = '';
-                    for ($iii = 0; $iii < count($split2); $iii ++) {
-                        if ($split2[$iii]) {
-                            $asis .= trim($split2[$iii]) . chr(10);
-                        }
-                        if ($minify_javascript != 'no') {
-                            if (strpos($split2[$iii], '//') !== false && substr(trim($split2[$iii]), -1) == ';') {
-                                $asis .= chr(10);
-                            }
-                        }
-                    }
-                    if ($asis) {
-                        $asis = substr($asis, 0, -1);
-                    }
-                    if ($minify_html_comments != 'no') {
-                        $asis = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $asis);
-                    }
-                    if ($minify_javascript != 'no') {
-                        $asis = str_replace(array(';' . chr(10), '>' . chr(10), '{' . chr(10), '}' . chr(10), ',' . chr(10)), array(';', '>', '{', '}', ','), $asis);
-                    }
-                } elseif (substr($asis, 0, 6) == '<style') {
-                    $asis = preg_replace(array('/\>[^\S ]+' . $mod, '/[^\S ]+\<' . $mod, '/(\s)+' . $mod), array('>', '<', '\\1'), $asis);
-                    if ($minify_html_comments != 'no') {
-                        $asis = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $asis);
-                    }
-                    $asis = str_replace(array(chr(10), ' {', '{ ', ' }', '} ', '( ', ' )', ' :', ': ', ' ;', '; ', ' ,', ', ', ';}'), array('', '{', '{', '}', '}', '(', ')', ':', ':', ';', ';', ',', ',', '}'), $asis);
-                }
-            } else {
-                $process = $split[$i];
-                $asis = '';
-            }
-            $process = preg_replace(array('/\>[^\S ]+' . $mod, '/[^\S ]+\<' . $mod, '/(\s)+' . $mod), array('>', '<', '\\1'), $process);
-            if ($minify_html_comments != 'no') {
-                $process = preg_replace('/<!--(?!\s*(?:\[if [^\]]+]|<!|>))(?:(?!-->).)*-->' . $mod, '', $process);
-            }
-            $buffer .= $process.$asis;
-        }
-        $buffer = str_replace(array(chr(10) . '<script', chr(10) . '<style', '*/' . chr(10), 'M1N1FY-ST4RT'), array('<script', '<style', '*/', ''), $buffer);
-        $minify_html_xhtml = get_option('minify_html_xhtml');
-        $minify_html_relative = get_option('minify_html_relative');
-        $minify_html_scheme = get_option('minify_html_scheme');
-        if ($minify_html_xhtml == 'yes' && strtolower(substr(ltrim($buffer), 0, 15)) == '<!doctype html>') {
-            $buffer = str_replace(' />', '>', $buffer);
-        }
-        if (!is_admin() && $minify_html_relative == 'yes') {
-            $buffer = str_replace(array( 'https://' . $_SERVER['HTTP_HOST'] . '/', 'http://' . $_SERVER['HTTP_HOST'] . '/', '//' . $_SERVER['HTTP_HOST'] . '/' ), array( '/', '/', '/' ), $buffer);
-        }
-        if (!is_admin() && $minify_html_scheme == 'yes') {
-            $buffer = str_replace(array( 'http://', 'https://' ), '//', $buffer);
-        }
-        return ($buffer);
+        $htmlMin = new HtmlMin();
+
+        // ACTIVE OPTIONS
+        $htmlMin->doOptimizeViaHtmlDomParser();               // optimize html via "HtmlDomParser()"
+        $htmlMin->doRemoveComments();                         // remove default HTML comments (depends on "doOptimizeViaHtmlDomParser(true)")
+        $htmlMin->doSumUpWhitespace();                        // sum-up extra whitespace from the Dom (depends on "doOptimizeViaHtmlDomParser(true)")
+        $htmlMin->doRemoveWhitespaceAroundTags();             // remove whitespace around tags (depends on "doOptimizeViaHtmlDomParser(true)")
+        $htmlMin->doOptimizeAttributes();                     // optimize html attributes (depends on "doOptimizeViaHtmlDomParser(true)")
+
+        // NON ACTIVE OPTIONS : test if active one of these line, may breal html output
+        // $htmlMin->doRemoveHttpPrefixFromAttributes();         // remove optional "http:"-prefix from attributes (depends on "doOptimizeAttributes(true)")
+        // $htmlMin->doRemoveDefaultAttributes();                // remove defaults (depends on "doOptimizeAttributes(true)" | disabled by default)
+        // $htmlMin->doRemoveDeprecatedAnchorName();             // remove deprecated anchor-jump (depends on "doOptimizeAttributes(true)")
+        // $htmlMin->doRemoveDeprecatedScriptCharsetAttribute(); // remove deprecated charset-attribute - the browser will use the charset from the HTTP-Header, anyway (depends on "doOptimizeAttributes(true)")
+        // $htmlMin->doRemoveDeprecatedTypeFromScriptTag();      // remove deprecated script-mime-types (depends on "doOptimizeAttributes(true)")
+        // $htmlMin->doRemoveDeprecatedTypeFromStylesheetLink(); // remove "type=text/css" for css links (depends on "doOptimizeAttributes(true)")
+        // $htmlMin->doRemoveEmptyAttributes();                  // remove some empty attributes (depends on "doOptimizeAttributes(true)")
+        // $htmlMin->doRemoveValueFromEmptyInput();              // remove 'value=""' from empty <input> (depends on "doOptimizeAttributes(true)")
+        // $htmlMin->doSortCssClassNames();                      // sort css-class-names, for better gzip results (depends on "doOptimizeAttributes(true)")
+        // $htmlMin->doSortHtmlAttributes();                     // sort html-attributes, for better gzip results (depends on "doOptimizeAttributes(true)")
+        // $htmlMin->doRemoveSpacesBetweenTags();                // remove more (aggressive) spaces in the dom (disabled by default)
+        // $htmlMin->doRemoveOmittedQuotes();                    // remove quotes e.g. class="lall" => class=lall
+        // $htmlMin->doRemoveOmittedHtmlTags();                  // remove ommitted html tags e.g. <p>lall</p> => <p>lall
+
+        return $htmlMin->minify($buffer);
     }
 }
