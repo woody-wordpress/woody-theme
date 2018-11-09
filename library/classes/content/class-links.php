9bcc2762a9819824955cc3b8584e4cf6e68a54e8
Léo POIROUX <leo.poiroux@gmail.com>
2018-11-09 16:42:41 +0100

    up

--- library/classes/content/class-links.php
+++ library/classes/content/class-links.php
@@ -20,14 +20,14 @@ class WoodyTheme_Links
 
     public function customLinksSearch($query)
     {
-        if (strpos($query['s'], '-page') !== false) {
-            $query['s'] = str_replace('-page', '', $query['s']);
+        if (strpos($query['s'], '#page') !== false) {
+            $query['s'] = str_replace('#page', '', $query['s']);
             $query['post_type'] = array('page');
-        } elseif (strpos($query['s'], '-sit') !== false) {
-            $query['s'] = str_replace('-sit', '', $query['s']);
+        } elseif (strpos($query['s'], '#sit') !== false) {
+            $query['s'] = str_replace('#sit', '', $query['s']);
             $query['post_type'] = array('touristic_sheet');
-        } elseif (strpos($query['s'], '-lien') !== false) {
-            $query['s'] = str_replace('-lien', '', $query['s']);
+        } elseif (strpos($query['s'], '#lien') !== false) {
+            $query['s'] = str_replace('#lien', '', $query['s']);
             $query['post_type'] = array('short_link');
         }
 
