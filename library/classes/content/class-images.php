35aa78f5a0a0c06bda1b4714da0223105db532a7
Léo POIROUX <leo.poiroux@gmail.com>
2018-11-26 17:29:09 +0100

    remove full size

--- library/classes/content/class-images.php
+++ library/classes/content/class-images.php
@@ -301,6 +301,9 @@ class WoodyTheme_Images
                     ];
                 }
             }
+
+            // Remove full size
+            unset($metadata['sizes']['full']);
         }
 
         return $metadata;
