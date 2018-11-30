e75ca7a69176fddd67c79fa814a68fb252ff1915
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2018-11-30 10:30:21 +0100

    updates

--- library/templates/template.php
+++ library/templates/template.php
@@ -146,6 +146,7 @@ abstract class WoodyTheme_TemplateAbstract
                 if(!empty($languages_customization['template'])){
                     $template = $languages_customization['template'];
                 }
+                $data['flags'] = (!empty($languages_customization['flags'])) ? $languages_customization['flags'] : false;
                 if(!empty($languages_customization['external_langs'])){
                     foreach ($languages_customization['external_langs'] as $lang_key => $language ) {
                         $data['langs'][$lang_key]['url'] = $language['url'];
