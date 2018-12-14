cf399a260a8284572c42daf90673947b6a3b2222
Léo POIROUX <leo.poiroux@gmail.com>
2018-12-14 10:06:49 +0100

    varnish

--- library/classes/plugins/class-plugins-options.php
+++ library/classes/plugins/class-plugins-options.php
@@ -469,7 +469,6 @@ class WoodyTheme_Plugins_Options
         update_option('varnish_caching_purge_key', 'l6ka6sb3hff9fzhx4h2qa38iqgyedznou5hawcj4rgfxlvx9m69zyqtz78yfsmws', '', 'yes');
         update_option('varnish_caching_cookie', 'y0ecy4qrkcw5rkfdyxyuf9dsoi62omz5fnpkdou8er5xcfeg7hvkqskyn7ps961j', '', 'yes');
         update_option('varnish_caching_debug', (WP_ENV == 'prod') ? false : true, '', 'yes');
-        update_option('varnish_caching_dynamic_host', true, '', 'yes');
     }
 
     private function updateOption($option_name, $settings, $autoload = 'yes')
