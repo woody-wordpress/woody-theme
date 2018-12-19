906763f099370ee2b81845fec2d95f67be71bfed
Léo POIROUX <leo.poiroux@gmail.com>
2018-12-19 11:47:46 +0100

    minify

--- library/classes/cleanup/class-minify.php
+++ library/classes/cleanup/class-minify.php
@@ -18,6 +18,7 @@ class WoodyTheme_Cleanup_Minify
     protected function registerHooks()
     {
         $minify_html_active = get_option('minify_html_active');
+        $minify_html_active = 'no';
         if (!(defined('WP_CLI') && WP_CLI) && $minify_html_active != 'no' && (!defined('DOING_AJAX') || !DOING_AJAX)) {
             add_action('init', [$this, 'minifyHtml'], 1);
         }
@@ -30,30 +31,8 @@ class WoodyTheme_Cleanup_Minify
 
     public function minifyHtmlOutput($buffer)
     {
-        $htmlMin = new HtmlMin();
-
-        // ACTIVE OPTIONS
-        $htmlMin->doOptimizeViaHtmlDomParser();               // optimize html via "HtmlDomParser()"
-        $htmlMin->doRemoveComments();                         // remove default HTML comments (depends on "doOptimizeViaHtmlDomParser(true)")
-        $htmlMin->doSumUpWhitespace();                        // sum-up extra whitespace from the Dom (depends on "doOptimizeViaHtmlDomParser(true)")
-        $htmlMin->doRemoveWhitespaceAroundTags();             // remove whitespace around tags (depends on "doOptimizeViaHtmlDomParser(true)")
-        $htmlMin->doOptimizeAttributes();                     // optimize html attributes (depends on "doOptimizeViaHtmlDomParser(true)")
-
-        // NON ACTIVE OPTIONS : test if active one of these line, may breal html output
-        // $htmlMin->doRemoveHttpPrefixFromAttributes();         // remove optional "http:"-prefix from attributes (depends on "doOptimizeAttributes(true)")
-        // $htmlMin->doRemoveDefaultAttributes();                // remove defaults (depends on "doOptimizeAttributes(true)" | disabled by default)
-        // $htmlMin->doRemoveDeprecatedAnchorName();             // remove deprecated anchor-jump (depends on "doOptimizeAttributes(true)")
-        // $htmlMin->doRemoveDeprecatedScriptCharsetAttribute(); // remove deprecated charset-attribute - the browser will use the charset from the HTTP-Header, anyway (depends on "doOptimizeAttributes(true)")
-        // $htmlMin->doRemoveDeprecatedTypeFromScriptTag();      // remove deprecated script-mime-types (depends on "doOptimizeAttributes(true)")
-        // $htmlMin->doRemoveDeprecatedTypeFromStylesheetLink(); // remove "type=text/css" for css links (depends on "doOptimizeAttributes(true)")
-        // $htmlMin->doRemoveEmptyAttributes();                  // remove some empty attributes (depends on "doOptimizeAttributes(true)")
-        // $htmlMin->doRemoveValueFromEmptyInput();              // remove 'value=""' from empty <input> (depends on "doOptimizeAttributes(true)")
-        // $htmlMin->doSortCssClassNames();                      // sort css-class-names, for better gzip results (depends on "doOptimizeAttributes(true)")
-        // $htmlMin->doSortHtmlAttributes();                     // sort html-attributes, for better gzip results (depends on "doOptimizeAttributes(true)")
-        // $htmlMin->doRemoveSpacesBetweenTags();                // remove more (aggressive) spaces in the dom (disabled by default)
-        // $htmlMin->doRemoveOmittedQuotes();                    // remove quotes e.g. class="lall" => class=lall
-        // $htmlMin->doRemoveOmittedHtmlTags();                  // remove ommitted html tags e.g. <p>lall</p> => <p>lall
-
-        return $htmlMin->minify($buffer);
+        $buffer = preg_replace('/<!--(.|\s)*?-->/', '', $buffer);
+        $buffer = trim(preg_replace('/>\s+</', '><', $buffer));
+        return $buffer;
     }
 }
