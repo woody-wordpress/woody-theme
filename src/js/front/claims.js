7a4a7e6fceeca3e094672a5a0de52badb524f28a
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2019-01-10 22:50:58 +0100

    simplification

--- src/js/front/claims.js
+++ src/js/front/claims.js
@@ -11,16 +11,12 @@ $('.claims-blocks-wrapper').each(function() {
             var item = data[Math.floor(Math.random() * data.length)];
             $this.append(item);
             var swiper_options = $this.find('.claims-swiper').data('options');
-            var claimSwiper = new Swiper('.claims-swiper', swiper_options);
-            $('.woody-component-claims-block').each(function() {
-                var $this = $(this),
-                    closeClaim = $this.find('.claim-close-button');
-                console.log(closeClaim);
-                // TODO: set cookie on click
-                closeClaim.click(function() {
-                    console.log($this);
-                    $this.remove();
-                });
+            var closeClaim = $this.find('.claim-close-button');
+            new Swiper('.claims-swiper', swiper_options);
+
+            // TODO: set cookie on click
+            closeClaim.click(function() {
+                $this.remove();
             });
         },
         error: function(data) {
