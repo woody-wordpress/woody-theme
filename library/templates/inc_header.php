79b060772cde1ee1a8f7d964139a5511a3989193
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2019-01-22 09:01:21 +0100

    inc url

--- library/templates/inc_header.php
+++ library/templates/inc_header.php
@@ -2,10 +2,10 @@
 
 $context = [
     'styles' => [
-        'header_resa' => 'background-color: #fff; height: 50px; padding: 15px; position:relative;',
+        'header_resa' => 'font-family:Helvetica,Arial, sans-serif; background-color: #fff; height: 65px; padding: 15px; position:relative; border-bottom:1px solid rgba(0,0,0,.1);',
         'header_resa_container' => 'max-width:1400px; margin:0 auto',
         'logo' => 'display: block; margin: 0 auto; height:100%',
-        'backhome' => 'color:rgba(0,0,0,.4); font-size: 14px; padding:10px; border:1px solid; position:absolute; left:0; top:50%; transform:translateY(-50%)'
+        'backhome' => 'color:rgba(0,0,0,.4); font-size: 12px; padding:8px 10px; border:1px solid rgba(0,0,0,.2); position:absolute; left:0; top:50%; transform:translateY(-50%); text-decoration:none'
     ],
     'logo_url' => (!empty(get_stylesheet_directory_uri() . '/logo.svg')) ? get_stylesheet_directory_uri() . '/logo.svg' : '',
     'home_url' => home_url()
