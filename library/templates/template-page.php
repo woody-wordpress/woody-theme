6402d9c20b52af608be4cd43c23d5d8b87cfb306
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2019-01-23 16:54:02 +0100

    hotfix

--- library/templates/template-page.php
+++ library/templates/template-page.php
@@ -136,7 +136,6 @@ class WoodyTheme_Template_Page extends WoodyTheme_TemplateAbstract
         $page_hero = [];
         $page_hero = getAcfGroupFields('group_5b052bbee40a4');
         if (!empty($page_hero['page_heading_media_type']) && ($page_hero['page_heading_media_type'] == 'movie' && !empty($page_hero['page_heading_movie']) || ($page_hero['page_heading_media_type'] == 'img' && !empty($page_hero['page_heading_img'])))) {
-            wd($page_teaser, 'Page teaser');
             if (empty($page_teaser['page_teaser_display_title'])) {
                 $page_hero['title_as_h1'] = true;
             }
