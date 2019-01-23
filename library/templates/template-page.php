320d0e494b42d412256b70f638fc46505b5e497e
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2019-01-23 16:29:09 +0100

    move visuel et accroche context to page context

--- library/templates/template-page.php
+++ library/templates/template-page.php
@@ -129,19 +129,6 @@ class WoodyTheme_Template_Page extends WoodyTheme_TemplateAbstract
 
             $this->context['page_teaser'] = Timber::compile($this->context['woody_components'][$page_teaser['page_teaser_woody_tpl']], $page_teaser);
         }
-    }
-
-    protected function commonContext()
-    {
-        $this->context['page_terms'] = implode(' ', getPageTerms($this->context['post']->ID));
-        $this->context['default_marker'] = file_get_contents($this->context['dist_dir'] . '/img/default-marker.svg');
-
-        /*********************************************
-         * Check type de publication
-         *********************************************/
-        if ($this->context['page_type'] === 'playlist_tourism') {
-            $this->playlistContext();
-        }
 
         /*********************************************
         * Compilation du visuel et accroche
@@ -149,6 +136,7 @@ class WoodyTheme_Template_Page extends WoodyTheme_TemplateAbstract
         $page_hero = [];
         $page_hero = getAcfGroupFields('group_5b052bbee40a4');
         if (!empty($page_hero['page_heading_media_type']) && ($page_hero['page_heading_media_type'] == 'movie' && !empty($page_hero['page_heading_movie']) || ($page_hero['page_heading_media_type'] == 'img' && !empty($page_hero['page_heading_img'])))) {
+            wd($page_teaser, 'Page teaser');
             if (empty($page_teaser['page_teaser_display_title'])) {
                 $page_hero['title_as_h1'] = true;
             }
@@ -166,6 +154,19 @@ class WoodyTheme_Template_Page extends WoodyTheme_TemplateAbstract
             $page_hero['title'] = (!empty($page_hero['title'])) ? str_replace('-', '&#8209', $page_hero['title']) : '';
 
             $this->context['page_hero'] = Timber::compile($this->context['woody_components'][$page_hero['heading_woody_tpl']], $page_hero);
+        }
+    }
+
+    protected function commonContext()
+    {
+        $this->context['page_terms'] = implode(' ', getPageTerms($this->context['post']->ID));
+        $this->context['default_marker'] = file_get_contents($this->context['dist_dir'] . '/img/default-marker.svg');
+
+        /*********************************************
+         * Check type de publication
+         *********************************************/
+        if ($this->context['page_type'] === 'playlist_tourism') {
+            $this->playlistContext();
         }
 
         /*********************************************
