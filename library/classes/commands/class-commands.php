c6c57001b1a58676ca7c56a8033c02a9c502506b
Léo POIROUX <leo.poiroux@gmail.com>
2019-01-28 12:12:39 +0100

    purge all domain on deploy

--- library/classes/commands/class-commands.php
+++ library/classes/commands/class-commands.php
@@ -5,6 +5,7 @@
  * @package WoodyTheme
  * @since WoodyTheme 1.0.0
  */
+use Woody\Utils\Output;
 
 class WoodyTheme_Commands
 {
@@ -21,17 +22,67 @@ class WoodyTheme_Commands
     public function flush($args)
     {
         do_action('woody_subtheme_update');
-        \WP_CLI::success('woody_subtheme_update');
+        Output::success('woody_subtheme_update');
 
         do_action('woody_theme_update');
-        \WP_CLI::success('woody_theme_update');
+        Output::success('woody_theme_update');
 
         // Clear the cache to prevent an update_option() from saving a stale db_version to the cache
         wp_cache_flush();
-        \WP_CLI::success('wp_cache_flush');
+        Output::success('wp_cache_flush');
 
         // (Not all cache back ends listen to 'flush')
         wp_cache_delete('alloptions', 'options');
-        \WP_CLI::success('wp_cache_delete alloptions');
+        Output::success('wp_cache_delete alloptions');
+
+        // (Not all cache back ends listen to 'flush')
+        $this->purge_varnish();
+    }
+
+    private function purge_varnish()
+    {
+        // Options
+        $vcaching_prefix = 'varnish_caching_';
+        $vcaching_useSsl = get_option($vcaching_prefix . 'ssl');
+        $vcaching_purgeKey = ($purgeKey = trim(get_option($vcaching_prefix . 'purge_key'))) ? $purgeKey : null;
+        $vcaching_varnishIp = get_option($vcaching_prefix . 'ips');
+        $vcaching_varnishIp = explode(',', $vcaching_varnishIp);
+        $vcaching_varnishIp = apply_filters('vcaching_varnish_ips', $vcaching_varnishIp);
+
+        // Get schema
+        $schema = apply_filters('vcaching_schema', $vcaching_useSsl ? 'https://' : 'http://');
+
+        // Get hosts
+        $hosts = [];
+        $polylang = get_option('polylang');
+        if ($polylang['force_lang'] == 3 && !empty($polylang['domains'])) {
+            foreach ($polylang['domains'] as $lang => $domain) {
+                $hosts[$lang] = parse_url($domain, PHP_URL_HOST);
+            }
+        } else {
+            $hosts['all'] = parse_url(WP_HOME, PHP_URL_HOST);
+        }
+
+        foreach ($hosts as $lang => $host) {
+            foreach ($vcaching_varnishIp as $ip) {
+                $purgeme = $schema . $ip . '/.*';
+                $headers = array('host' => $host, 'X-VC-Purge-Method' => 'regex', 'X-VC-Purge-Host' => $host);
+                if (!is_null($vcaching_purgeKey)) {
+                    $headers['X-VC-Purge-Key'] = $vcaching_purgeKey;
+                }
+                $response = wp_remote_request($purgeme, array('method' => 'PURGE', 'headers' => $headers, "sslverify" => false));
+                if ($response instanceof WP_Error) {
+                    foreach ($response->errors as $error => $errors) {
+                        $noticeMessage .= 'Error ' . $error . ' : ';
+                        foreach ($errors as $error => $description) {
+                            $noticeMessage .= ' - ' . $description;
+                        }
+                        Output::error(['wp_varnish_purge' => $noticeMessage]);
+                    }
+                } else {
+                    Output::success(sprintf('wp_varnish_purge %s => %s (%s)', $purgeme, $host, $lang));
+                }
+            }
+        }
     }
 }
