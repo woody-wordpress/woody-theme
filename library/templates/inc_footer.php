60bccea5a2940191a7e245e889e1ae0837356a48
Antoine Josset <antoine.josset@raccourci.fr>
2019-04-09 17:58:31 +0200

    up inclusions

--- library/templates/inc_footer.php
+++ library/templates/inc_footer.php
@@ -3,7 +3,7 @@
 $context = [
     'styles' => [
         'footer_resa' => 'font-family:Helvetica,Arial, sans-serif; background-color: #eaeaea; height: 85px; padding: 15px; position:relative; border-top:1px solid rgba(0,0,0,.1);',
-        'header_resa_container' => 'max-width:1400px; margin:0 auto',
+        'footer_resa_container' => 'max-width:1400px; margin:0 auto',
         'logo' => 'display: block; margin: 0 auto; height:100%',
     ],
     'logo_url' => (!empty(get_stylesheet_directory_uri() . '/logo.svg')) ? get_stylesheet_directory_uri() . '/logo.svg' : '',
