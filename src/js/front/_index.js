e53a6c56ccc3297e19c97f91af317c3b8ffda956
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2019-06-17 17:33:51 +0200

    change libs load order

--- src/js/front/_index.js
+++ src/js/front/_index.js
@@ -1,5 +1,5 @@
-import 'woody-library/assets/js/_index';
 import './framework/foundation-explicit-pieces';
+import 'woody-library/assets/js/_index';
 import './plugins/cookieconsent';
 import './plugins/scroll_to_top';
 
