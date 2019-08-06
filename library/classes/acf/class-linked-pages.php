ccfd2ac9127125b362b66f492476fef8a572f333
LEGENDRE <jeremy.legendre@raccourci.fr>
2019-08-06 09:40:28 +0200

    load field values for linked page

--- library/classes/acf/class-linked-pages.php
+++ library/classes/acf/class-linked-pages.php
@@ -16,7 +16,83 @@ class WoodyTheme_ACF_LinkedPages
 
     protected function registerHooks()
     {
-        add_action('wp_ajax_woody_autofocus_count', [$this, 'autoFocusCount']);
+        add_action('wp_ajax_woody_get_available_link_page', [$this, 'getAvailablePages']);
     }
 
+    public function getAvailablePages()
+    {
+
+        $return = [];
+        $value = filter_input(INPUT_POST, 'params', FILTER_SANITIZE_STRING);
+        $post_id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
+        $selected = !empty(get_post_meta($post_id, 'linked_alternative_page')) ? get_post_meta($post_id, 'linked_alternative_page')[0] : '';
+
+
+        $return[] = [
+            'id' => intval($selected),
+            'title' => get_the_title($selected),
+            'selected' => true
+        ];
+
+        if (!empty($value)) {
+            $args = array(
+                'post_status' => array(
+                    'draft',
+                    'publish'
+                ),
+                'posts_per_page' => -1,
+                'post_type' => 'page',
+                'meta_query' => array(
+                    'relation' => 'AND',
+                    array(
+                        'relation' => 'OR',
+                        array(
+                            'key' => 'choice_prepare_on_the_spot',
+                            'value' => $value,
+                            'compare' => '!='
+                        ),
+                        array(
+                            'key' => 'choice_prepare_on_the_spot',
+                            'compare' => 'NOT EXISTS',
+                            'value' => ''
+                        ),
+                    ),
+                    array(
+                        'key' => 'linked_alternative_page',
+                        'compare' => 'NOT EXISTS',
+                        'value' => ''
+                    )
+                ),
+                'post__not_in' => array(
+                    $post_id
+                )
+            );
+
+            $query_result = new \WP_Query($args);
+
+            if (!empty($query_result->posts)) {
+                foreach ($query_result->posts as $post) {
+                    $title = !empty($post->post_title) ? $post->post_title : 'Sans titre';
+
+                    $return[] = [
+                        'id'=> $post->ID,
+                        'title'=> $title,
+                        'selected'=> false
+                    ];
+                }
+            }
+        }
+
+        $this->JsonResponse($return);
+    }
+
+    private function JsonResponse($response)
+    {
+        if (!is_null($response)) {
+            wp_send_json($response);
+        } else {
+            header("HTTP/1.0 400 Bad Request");
+            die();
+        }
+    }
 }
--- src/js/admin/prepare_onspot.js
+++ src/js/admin/prepare_onspot.js
@@ -1,25 +1,42 @@
 import $ from 'jquery';
 
-var setChoicesLinkToPage = function(field){
-    let page_type = $(this).prev().find('.prepare-onspot .acf-input ul li .selected');
-    console.log(page_type);
-    // $.ajax({
-    //     type: 'POST',
-    //     dataType: 'json',
-    //     url: ajaxurl,
-    //     data: {
-    //         action: 'woody_get_available_link_page',
-    //         params: ''
-    //     },
-    //     success: function(response){
-    //         console.log(response);
-    //     }
-    // });
-}
+var setChoicesLinkToPage = function(value){
+    let page_type = '';
+    let post_id = $('#post_ID').val();
+    if (value == "spot" || value == "prepare") {
+        page_type = value;
+    } else {
+        page_type = value.$el.prev().find('.acf-input .selected input').val();
+    }
 
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        url: ajaxurl,
+        data: {
+            action: 'woody_get_available_link_page',
+            params: page_type,
+            id: post_id
+        },
+        success: function(response){
+            $('#acf-field_5d47d332df765 option').each(function(){
+                $(this).remove();
+            });
+            response.forEach(function(element){
+                if(element['selected'] == true){
+                    var option = '<option value="'+ element['id'] +'" selected="selected">'+ element['title'] +'</option>';
+                }else{
+                    var option = '<option value="'+ element['id'] +'">'+ element['title'] +'</option>';
+                }
+                $('#acf-field_5d47d332df765').append(option);
+            });
+        }
+    });
+}
 
 acf.addAction('load_field/name=linked_alternative_page', setChoicesLinkToPage);
 
 $('.prepare-onspot').on('change', function(){
-    console.log($(this).find('.acf-input ul li .selected'));
+    let page_type = $(this).find('.acf-input .selected input').val();
+    setChoicesLinkToPage(page_type);
 });
