5445ad4f219dda3ecc27f7dda916f58308764e33
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2019-09-03 09:18:20 +0200

    ajax search

--- src/js/front/_index.js
+++ src/js/front/_index.js
@@ -2,6 +2,7 @@ import 'woody-library/assets/js/_index';
 import './framework/foundation-explicit-pieces';
 import './plugins/cookieconsent';
 import './plugins/scroll_to_top';
+import './plugins/search';
 
 // Woody Plugin
 import 'Claims/Resources/Assets/js/claims'
---
+++ src/js/front/plugins/search.js
@@ -0,0 +1,69 @@
+import $ from 'jquery';
+
+$('.woody-component-esSearch').each(function() {
+    var $this = $(this),
+        $input = $this.find('.woody-esForm input[type="text"]'),
+        $listWrapper = $this.find('.list-wrapper'),
+        $loader = $this.find('.ajaxloader'),
+        $label = $this.find('.input-group-label'),
+        currentUrl = window.location.protocol + '//' + window.location.host + window.location.pathname,
+        xhr = null;
+
+    function debounce(func, wait, immediate) {
+        var timeout;
+        return function() {
+            var context = this,
+                args = arguments;
+            var later = function() {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    };
+
+    var ajax_search = function() {
+        xhr = $.ajax({
+            type: 'GET',
+            url: currentUrl,
+            data: 'query=' + $input.val(),
+            beforeSend: function() {
+                if (xhr != null) {
+                    xhr.abort();
+                }
+            },
+            success: function(html) {
+                var results = $(html).find('.woody-component-esSearch .list-wrapper').html();
+                $listWrapper.html(results);
+                window.history.pushState({ query: $input.val() }, null, currentUrl + '?query=' + $input.val());
+                $label.removeClass('hide');
+                $loader.addClass('hide');
+            },
+            error: function() {
+                console.error('search failed');
+            },
+        });
+    }
+
+    window.addEventListener('popstate', function(e) {
+        var state = e.state;
+        if (state == null) {
+            $input.val('');
+        } else {
+            $input.val(state.query);
+            ajax_search();
+        }
+    });
+
+    $input.keyup(function() {
+        $label.addClass('hide');
+        $loader.removeClass('hide');
+    });
+
+    $input.keyup(debounce(function() {
+        ajax_search();
+    }, 1000));
+});
