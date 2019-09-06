4979f3e15e7a669b326c229fa9c2872c9ae8a667
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2019-09-06 09:39:48 +0200

    bugfix

--- library/templates/template-page.php
+++ library/templates/template-page.php
@@ -164,11 +164,16 @@ class WoodyTheme_Template_Page extends WoodyTheme_TemplateAbstract
                 $page_teaser['the_classes'][] = (!empty($page_teaser['teaser_margin_bottom'])) ? $page_teaser['teaser_margin_bottom'] : '';
                 $page_teaser['the_classes'][] = (!empty($page_teaser['background_img'])) ? 'isRel' : '';
                 $page_teaser['classes'] = (!empty($page_teaser['the_classes'])) ? implode(' ', $page_teaser['the_classes']) : '';
-                $page_teaser['breadcrumb'] = (!empty($this->context['hide_page_zones']) && !in_array('breadcrumb', $this->context['hide_page_zones'])) ? yoast_breadcrumb('<div class="breadcrumb-wrapper padd-all-sm">', '</div>', false) : null;
+                $page_teaser['breadcrumb'] = yoast_breadcrumb('<div class="breadcrumb-wrapper padd-all-sm">', '</div>', false);
                 $page_teaser['trip_infos'] = (!empty($this->context['trip_infos'])) ? $this->context['trip_infos'] : '';
                 $page_teaser['social_shares'] = (!empty($this->context['social_shares'])) ? $this->context['social_shares'] : '';
                 if (!empty($page_teaser['page_teaser_media_type']) && $page_teaser['page_teaser_media_type'] == 'map') {
                     $page_teaser['post_coordinates'] = (!empty(getAcfGroupFields('group_5b3635da6529e', $this->context['post']))) ? getAcfGroupFields('group_5b3635da6529e', $this->context['post']) : '';
+                }
+
+                // Unset breadcrumb if checked in hide page zones options
+                if (!empty($this->context['hide_page_zones']) && in_array('breadcrumb', $this->context['hide_page_zones'])) {
+                    unset($page_teaser['breadcrumb']);
                 }
 
                 $this->context['page_teaser'] = Timber::compile($this->context['woody_components'][$page_teaser['page_teaser_woody_tpl']], $page_teaser);
