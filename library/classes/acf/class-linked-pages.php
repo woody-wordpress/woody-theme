791e360b37f8db67b48abbad97c4a8467aeb9da3
LEGENDRE <jeremy.legendre@raccourci.fr>
2019-09-17 10:55:07 +0200

    load linked_pages field on switch change

--- library/classes/acf/class-linked-pages.php
+++ library/classes/acf/class-linked-pages.php
@@ -19,11 +19,11 @@ class WoodyTheme_ACF_LinkedPages
 
     protected function registerHooks()
     {
-
-        add_action('init', [$this, 'registerTaxonomy'] );
+        add_action('init', [$this, 'registerTaxonomy']);
         add_action('woody_theme_update', [$this, 'setPostTaxonomyTerm']);
         add_action('acf/load_field/key=field_5d7f57f2b21f7', [$this, 'getAvailablePages'], 10, 1);
         add_action('save_post', [$this, 'setLinkBetweenPages'], 100, 1);
+        add_action('wp_ajax_set_post_term', [$this, 'setPostTerms']);
     }
 
     public function registerTaxonomy()
@@ -57,16 +57,17 @@ class WoodyTheme_ACF_LinkedPages
 
         $result = new \WP_Query($args);
 
-        foreach($result->posts as $post){
+        foreach ($result->posts as $post) {
             $type = get_field('field_5d47d14bdf764', $post->ID) ? 'prepare' : 'spot' ;
             $term = get_term_by('slug', $type, 'prepare_onspot');
-            wp_set_post_terms( $post->ID, $term->slug, 'prepare_onspot' );
+            wp_set_post_terms($post->ID, $term->slug, 'prepare_onspot');
         }
     }
 
     public function getAvailablePages($field)
     {
-        $type = get_field('field_5d47d14bdf764') ? 'spot' : 'prepare' ;
+        $post_id = filter_input(INPUT_POST, 'post_id', FILTER_VALIDATE_INT);
+        $type = get_field('field_5d47d14bdf764', $post_id) ? 'spot' : 'prepare' ;
         $field['taxonomy'] = ["prepare_onspot:".$type];
 
         return $field;
@@ -77,27 +78,38 @@ class WoodyTheme_ACF_LinkedPages
         $type = get_field('field_5d47d14bdf764', $post_id) ;
         $type_value = $type ? 'prepare' : 'spot';
         $term = get_term_by('slug', $type_value, 'prepare_onspot');
-        wp_set_post_terms( $post_id, $term->term_id, 'prepare_onspot' );
+        wp_set_post_terms($post_id, $term->slug, 'prepare_onspot');
 
-        if(!wp_is_post_revision( $post_id )){
+        if (!wp_is_post_revision($post_id)) {
             $opposite = false;
-            if(!empty($type)){
+            if (!empty($type)) {
                 // set linked page opposite (if current is preparation post, other must be on spot page )
                 $opposite = $type == true ? false : true ;
             }
 
             $linked_post = get_field('field_5d7f57f2b21f7', $post_id);
             $linked_post_related = $linked_post ? get_field('field_5d7f57f2b21f7', $linked_post->ID) : false ;
-            if($linked_post && !$linked_post_related ){
+            if ($linked_post && !$linked_post_related) {
                 update_field('field_5d47d14bdf764', $opposite, $linked_post->ID);
                 update_field('field_5d7f57f2b21f7', get_post($post_id), $linked_post->ID);
 
                 $type_value = $opposite ? 'prepare' : 'spot';
                 $term = get_term_by('slug', $type_value, 'prepare_onspot');
-                wp_set_post_terms( $linked_post->ID, $term, 'prepare_onspot' );
+                wp_set_post_terms($linked_post->ID, $term->slug, 'prepare_onspot');
             }
-        }else{
-
+        } else {
+            // TODO: remove old link
         }
+    }
+
+    public function setPostTerms()
+    {
+        $data = filter_input(INPUT_POST, 'params');
+        $post_id = filter_input(INPUT_POST, 'post_id');
+
+        $value = $data == 'prepare' ? true : false ;
+        $update = update_field('field_5d47d14bdf764', $value, $post_id);
+
+        wp_send_json($update);
     }
 }
--- src/js/admin/prepare_onspot.js
+++ src/js/admin/prepare_onspot.js
@@ -1,5 +1,22 @@
 import $ from 'jquery';
 
-$('#acf-field_5d47d14bdf764').on('change', function(){
-});
+$('#acf-field_5d47d14bdf764').on('change', function() {
+    var data = $(this).attr('checked') !== undefined && $(this).attr('checked') == "checked" ? 'prepare' : 'spot' ;
+    var post_id = $('#post_ID').val();
+    // AJAX CALL to set post term
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        url: ajaxurl,
+        data: {
+            action: 'set_post_term',
+            params: data,
+            post_id: post_id
+        },
+        success: function ( data )
+        {
 
+        },
+        error: function () { },
+    });
+});
