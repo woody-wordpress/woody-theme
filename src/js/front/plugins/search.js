a86b0bf5e2697f09fc30fa9b66588b7444bd41f1
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2019-09-24 12:00:43 +0200

    Allow searching by tags with ajax

--- src/js/front/plugins/search.js
+++ src/js/front/plugins/search.js
@@ -3,11 +3,38 @@ import $ from 'jquery';
 $('.woody-component-esSearch').each(function() {
     var $this = $(this),
         $input = $this.find('.woody-esForm input[type="text"]'),
+        $tags = $this.find('.search-tags-container .search-tag'),
         $listWrapper = $this.find('.list-wrapper'),
         $loader = $this.find('.ajaxloader'),
         $label = $this.find('.input-group-label'),
         currentUrl = window.location.protocol + '//' + window.location.host + window.location.pathname,
         xhr = null;
+
+    // Set the query with input field
+    var query = 'query=' + $input.val();
+
+    function getCheckedTags() {
+
+        // Get every .search-tag:checked to add to the query
+        var $activeTags = $this.find(('.search-tags-container .search-tag:checked'));
+        if ($activeTags.length) {
+            var tagString = '',
+                $i = 1;
+
+            $activeTags.each(function() {
+                var $activeTag = $(this);
+
+                if ($i > 1) {
+                    tagString += ':' + $activeTag.val();
+                } else {
+                    tagString += $activeTag.val();
+                }
+                $i++
+            });
+
+            query = 'query=' + $input.val() + '&tags=' + tagString;
+        }
+    }
 
     function debounce(func, wait, immediate) {
         var timeout;
@@ -29,7 +56,7 @@ $('.woody-component-esSearch').each(function() {
         xhr = $.ajax({
             type: 'GET',
             url: currentUrl,
-            data: 'query=' + $input.val(),
+            data: query,
             beforeSend: function() {
                 if (xhr != null) {
                     xhr.abort();
@@ -38,13 +65,13 @@ $('.woody-component-esSearch').each(function() {
             success: function(html) {
                 var results = $(html).find('.woody-component-esSearch .list-wrapper').html();
                 $listWrapper.html(results);
-                window.history.pushState({ query: $input.val() }, null, currentUrl + '?query=' + $input.val());
+                window.history.pushState({ query: $input.val() }, null, currentUrl + '?' + query);
                 $label.removeClass('hide');
                 $loader.addClass('hide');
                 $('html, body').animate({ scrollTop: 0 }, 100, 'linear');
             },
             error: function() {
-                console.error('search failed');
+                console.error('search aborted');
             },
         });
     }
@@ -64,7 +91,18 @@ $('.woody-component-esSearch').each(function() {
         $loader.removeClass('hide');
     });
 
+    $tags.click(function() {
+        $label.addClass('hide');
+        $loader.removeClass('hide');
+    });
+
     $input.keyup(debounce(function() {
+        getCheckedTags();
         ajax_search();
-    }, 1000));
+    }, 300));
+
+    $tags.click(debounce(function() {
+        getCheckedTags();
+        ajax_search();
+    }, 300));
 });
