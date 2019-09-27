b1a7aed0b5583c230becfff236f58eb7517488fd
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2019-09-27 16:20:48 +0200

    Bugfix  blank enhanced media buttons

--- src/js/admin/global-admin.js
+++ src/js/admin/global-admin.js
@@ -3,3 +3,8 @@ import $ from 'jquery';
 $('body.themes-php').each(function() {
     $('.theme-actions').remove();
 });
+
+// Bugfix  Enhanced media buttons
+window.wp.media.view.l10n.cancelSelection = window.wp.media.view.l10n.cancel;
+window.wp.media.view.l10n.trashSelected = window.wp.media.view.l10n.trash;
+window.wp.media.view.l10n.deleteSelected = window.wp.media.view.l10n.deletePermanently;
