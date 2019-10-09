4c6531c5282482f3bc783e38edbfff9406dfd4a0
LEGENDRE <jeremy.legendre@raccourci.fr>
2019-10-09 17:26:10 +0200

    calculate if coordinate is inside circle

--- src/js/front/plugins/prepare_onspot.js
+++ src/js/front/plugins/prepare_onspot.js
@@ -19,37 +19,73 @@ $('.tools .prepare_onspot_switcher input').on('click', function() {
 });
 
 // TODO: Get destination location
-// Get it from ERP ?
 
-// TODO: Get user location + create pop up
-// Can't work on http.
-var opt = {
-    timeout: 5000,
-    maximumAge: 0
+var dest_coord = {
+    latitude: 51.507351,
+    longitude: -0.127758
 };
 
-function success(pos) {
-    var crd = pos.coords;
+// TODO: Get user location + create pop up
+// var opt = {
+//     timeout: 5000,
+//     maximumAge: 0
+// };
 
-    console.log('Position actuelle :');
-    console.log('Latitude : ' + crd.latitude);
-    console.log('Longitude : ' + crd.longitude);
-    console.log('Précision : ' + crd.accuracy + ' mètres.');
+// function success(pos) {
+//    var crd = pos.coords;
 
-    // TODO: check if current position is within area
-    if (isInArea(crd)) {
-        // TODO: Show pop-up
-    }
-}
+//     console.log('Position actuelle :');
+//     console.log('Latitude : ' + crd.latitude);
+//     console.log('Longitude : ' + crd.longitude);
+//     console.log('Précision : ' + crd.accuracy + ' mètres.');
 
-function error(err) {
-    console.warn(`ERREUR (${err.code}): ${err.message}`);
-}
+//     if (isInArea(crd)) {
+//     }
+// }
 
-navigator.geolocation.getCurrentPosition(success, error, opt);
+// function error(err) {
+//     console.warn(`ERREUR (${err.code}): ${err.message}`);
+// }
 
-function isInArea(coord) {
+// navigator.geolocation.getCurrentPosition(success, error, opt);
+
+var coord = {
+    latitude: 51.619722,
+    longitude: 0.079651,
+    accuracy: 100
+};
+
+isInArea(coord, dest_coord);
+
+function isInArea(coord, dest_coord) {
     var is_inside = false;
 
+    var radius = 30;
+    var distance = calculDistance(coord, dest_coord );
+
+    if(distance <= radius){
+        is_inside = true;
+    }
+
     return is_inside;
+}
+
+// Convert Degrees into radian value
+function degreesToRadian(degrees){
+    return degrees * Math.PI / 180;
+}
+
+// Calcul distance between two coordinates. If distance is under radius, then it's inside the circle
+function calculDistance(coord, dest_coord){
+    var R = 6371; // Earth's Radius in Km
+
+    var dLat = degreesToRadian(dest_coord.latitude - coord.latitude);
+    var dLon = degreesToRadian(dest_coord.longitude - coord.longitude);
+
+    var lat1 = degreesToRadian(coord.latitude);
+    var lat2 = degreesToRadian(dest_coord.latitude);
+
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+    var c = 2 * Math.atan(Math.sqrt(a), Math.sqrt(1-a));
+    return R * c;
 }
