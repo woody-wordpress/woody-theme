bda3cf21e2cdd39786899e01430c1ca70f505391
LEGENDRE <jeremy.legendre@raccourci.fr>
2019-10-09 17:53:07 +0200

    firstvisit + beautify

--- src/js/front/plugins/prepare_onspot.js
+++ src/js/front/plugins/prepare_onspot.js
@@ -1,5 +1,13 @@
 import $ from 'jquery';
 
+$(document).ready(function() {
+    if (!getCookie('firstvisit')) {
+        // Show pop up ...
+        // $('.first_visit').show();
+        setCookie('firstvisit', true);
+    }
+});
+
 $('.tools .prepare_onspot_switcher input').on('click', function() {
     var switcher = $(this).prop('checked');
 
@@ -18,14 +26,11 @@ $('.tools .prepare_onspot_switcher input').on('click', function() {
     });
 });
 
-// TODO: Get destination location
-
 var dest_coord = {
     latitude: 51.507351,
     longitude: -0.127758
 };
 
-// TODO: Get user location + create pop up
 // var opt = {
 //     timeout: 5000,
 //     maximumAge: 0
@@ -55,15 +60,13 @@ var coord = {
     accuracy: 100
 };
 
-isInArea(coord, dest_coord);
-
 function isInArea(coord, dest_coord) {
     var is_inside = false;
 
     var radius = 30;
-    var distance = calculDistance(coord, dest_coord );
+    var distance = calculDistance(coord, dest_coord);
 
-    if(distance <= radius){
+    if (distance <= radius) {
         is_inside = true;
     }
 
@@ -71,12 +74,12 @@ function isInArea(coord, dest_coord) {
 }
 
 // Convert Degrees into radian value
-function degreesToRadian(degrees){
+function degreesToRadian(degrees) {
     return degrees * Math.PI / 180;
 }
 
 // Calcul distance between two coordinates. If distance is under radius, then it's inside the circle
-function calculDistance(coord, dest_coord){
+function calculDistance(coord, dest_coord) {
     var R = 6371; // Earth's Radius in Km
 
     var dLat = degreesToRadian(dest_coord.latitude - coord.latitude);
@@ -85,7 +88,7 @@ function calculDistance(coord, dest_coord){
     var lat1 = degreesToRadian(coord.latitude);
     var lat2 = degreesToRadian(dest_coord.latitude);
 
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
-    var c = 2 * Math.atan(Math.sqrt(a), Math.sqrt(1-a));
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+    var c = 2 * Math.atan(Math.sqrt(a), Math.sqrt(1 - a));
     return R * c;
 }
