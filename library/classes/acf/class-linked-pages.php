4679c0baf660f9918150b478809258fe5130334c
LEGENDRE <jeremy.legendre@raccourci.fr>
2019-10-10 14:10:40 +0200

    on first visit see if user is near dest_coord

--- library/classes/acf/class-linked-pages.php
+++ library/classes/acf/class-linked-pages.php
@@ -27,6 +27,7 @@ class WoodyTheme_ACF_LinkedPages
 
         add_action('wp_ajax_set_post_term', [$this, 'setPostTerms']);
         add_action('wp_ajax_redirect_prepare_onspot', [$this, 'redirectToLinkedPage']);
+        add_action('wp_ajax_get_destination_coord', [$this, 'getDestinationCoordinates']);
     }
 
     public function registerTaxonomy()
@@ -47,6 +48,9 @@ class WoodyTheme_ACF_LinkedPages
         wp_insert_term('spot', 'prepare_onspot', array('slug' => 'prepare'));
     }
 
+    /**
+     * Set all pages default value on woody theme update
+     */
     public function setPostTaxonomyTerm()
     {
         $args = array(
@@ -67,6 +71,11 @@ class WoodyTheme_ACF_LinkedPages
         }
     }
 
+    /**
+     * Set taxonomy term for wp query
+     * @param field
+     * @return field with tax value updated
+     */
     public function getAvailablePages($field)
     {
         $post_id = filter_input(INPUT_POST, 'post_id', FILTER_VALIDATE_INT);
@@ -76,6 +85,12 @@ class WoodyTheme_ACF_LinkedPages
         return $field;
     }
 
+    /**
+     * Remove link between two pages
+     * @param post_id
+     * @param post_after
+     * @param post_before post before save post
+     */
     public function removeLinkBetweenPages($post_id, $post_after, $post_before)
     {
         $old_linked_post = get_field('field_5d7f57f2b21f7', $post_before);
@@ -84,6 +99,10 @@ class WoodyTheme_ACF_LinkedPages
         }
     }
 
+    /**
+     * Update Link between pages
+     * @param post_id
+     */
     public function setLinkBetweenPages($post_id)
     {
         $type = get_field('field_5d47d14bdf764', $post_id) ;
@@ -114,6 +133,11 @@ class WoodyTheme_ACF_LinkedPages
         }
     }
 
+    /**
+     * Update post taxonomy (prepare OR spot)
+     * AJAX Call on switcher change (Back Office)
+     * Usefull for load_field field_5d7f57f2b21f7 ( retreive posts based on current value of prepare/onspot switcher)
+     */
     public function setPostTerms()
     {
         $data = filter_input(INPUT_POST, 'params');
@@ -125,6 +149,10 @@ class WoodyTheme_ACF_LinkedPages
         wp_send_json($update);
     }
 
+    /**
+     * Return url of the linked page if she exists on click on switcher
+     * AJAX call on swicther change (Front-End)
+     */
     public function redirectToLinkedPage()
     {
         $switcher = filter_input(INPUT_POST, 'params', FILTER_VALIDATE_BOOLEAN);
@@ -137,5 +165,22 @@ class WoodyTheme_ACF_LinkedPages
             wp_send_json($permalink);
         }
         exit;
+    }
+
+    /**
+     * Get website destination coordinates (latitude and longitude)
+     * AJAX Call on first visit of website, to see if user is near destination
+     */
+    public function getDestinationCoordinates()
+    {
+        $coord = [];
+        // TODO: get woody global vars...
+        // $coord['lat'] = WOODY_DESTINATION_LATITUDE;
+        // $coord['lon'] = WOODY_DESTINATION_LONGITUDE;
+
+        $coord['lat'] = 51.507351;
+        $coord['lon'] = -0.127758;
+
+        wp_send_json($coord);
     }
 }
--- src/js/front/plugins/prepare_onspot.js
+++ src/js/front/plugins/prepare_onspot.js
@@ -2,79 +2,99 @@ import $ from 'jquery';
 
 $(document).ready(function() {
     if (!Cookies.get('firstvisit')) {
-        // Show pop up ...
-        // $('.first_visit').show();
-        alert('first visit');
+
+        var dest_coord = {
+            latitude:0,
+            longitude:0,
+        };
+
+        // TODO: use google function for that
+        var coord = {
+            latitude: 51.619722,
+            longitude: 0.079651,
+            accuracy: 100
+        };
+
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            url: frontendajax.ajaxurl,
+            data: {
+                action: 'get_destination_coord',
+            },
+            success: function(response) {
+                dest_coord.latitude = parseFloat(response.lat);
+                dest_coord.longitude =  parseFloat(response.lon);
+
+                if (isInArea(coord, dest_coord)) {
+                    //    Show pop up ...
+                    console.log('is inside');
+                    alert('show pop up !');
+                }
+            }
+        });
+
+        // var opt = {
+        //     timeout: 5000,
+        //     maximumAge: 0
+        // };
+
+        // function success(pos) {
+        //    var crd = pos.coords;
+
+        //     console.log('Position actuelle :');
+        //     console.log('Latitude : ' + crd.latitude);
+        //     console.log('Longitude : ' + crd.longitude);
+        //     console.log('Précision : ' + crd.accuracy + ' mètres.');
+
+        //     if (isInArea(crd, dest_coord)) {
+        //            Show pop up ...
+        //            $('.first_visit').show();
+        //     }
+        // }
+
+        // function error(err) {
+        //     console.warn(`ERREUR (${err.code}): ${err.message}`);
+        // }
+
+        // navigator.geolocation.getCurrentPosition(success, error, opt);
+
+        function isInArea(coord, dest_coord) {
+            var is_inside = false;
+
+            var radius = 30;
+            var distance = calculDistance(coord, dest_coord);
+
+            if (distance <= radius) {
+                is_inside = true;
+            }
+
+            return is_inside;
+        }
+
+        // Convert Degrees into radian value
+        function degreesToRadian(degrees) {
+            return degrees * Math.PI / 180;
+        }
+
+        // Calcul distance between two coordinates. If distance is under radius, then it's inside the circle
+        function calculDistance(coord, dest_coord) {
+            var R = 6371; // Earth's Radius in Km
+
+            var dLat = degreesToRadian(dest_coord.latitude - coord.latitude);
+            var dLon = degreesToRadian(dest_coord.longitude - coord.longitude);
+
+            var lat1 = degreesToRadian(coord.latitude);
+            var lat2 = degreesToRadian(dest_coord.latitude);
+
+            var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+            var c = 2 * Math.atan(Math.sqrt(a), Math.sqrt(1 - a));
+            return R * c;
+        }
+
         Cookies.set('firstvisit', true, {expires: 5});
     }
 });
-
-// var opt = {
-//     timeout: 5000,
-//     maximumAge: 0
-// };
-
-// function success(pos) {
-//    var crd = pos.coords;
-
-//     console.log('Position actuelle :');
-//     console.log('Latitude : ' + crd.latitude);
-//     console.log('Longitude : ' + crd.longitude);
-//     console.log('Précision : ' + crd.accuracy + ' mètres.');
-
-//     if (isInArea(crd)) {
-//     }
-// }
-
-// function error(err) {
-//     console.warn(`ERREUR (${err.code}): ${err.message}`);
-// }
-
-// navigator.geolocation.getCurrentPosition(success, error, opt);
-
-var dest_coord = {
-    latitude: 51.507351,
-    longitude: -0.127758
-};
-
-var coord = {
-    latitude: 51.619722,
-    longitude: 0.079651,
-    accuracy: 100
-};
-
-function isInArea(coord, dest_coord) {
-    var is_inside = false;
-
-    var radius = 30;
-    var distance = calculDistance(coord, dest_coord);
-
-    if (distance <= radius) {
-        is_inside = true;
-    }
-
-    return is_inside;
-}
-
-// Convert Degrees into radian value
-function degreesToRadian(degrees) {
-    return degrees * Math.PI / 180;
-}
-
-// Calcul distance between two coordinates. If distance is under radius, then it's inside the circle
-function calculDistance(coord, dest_coord) {
-    var R = 6371; // Earth's Radius in Km
-
-    var dLat = degreesToRadian(dest_coord.latitude - coord.latitude);
-    var dLon = degreesToRadian(dest_coord.longitude - coord.longitude);
-
-    var lat1 = degreesToRadian(coord.latitude);
-    var lat2 = degreesToRadian(dest_coord.latitude);
-
-    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-    var c = 2 * Math.atan(Math.sqrt(a), Math.sqrt(1 - a));
-    return R * c;
-}
 
 /**
  * Redirect function
