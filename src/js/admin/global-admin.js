a4130d6a6963bc2b5d5c317a8de0c97467282be4
LEGENDRE <jeremy.legendre@raccourci.fr>
2019-10-23 09:58:46 +0200

    add apply tags button

--- src/js/admin/global-admin.js
+++ src/js/admin/global-admin.js
@@ -10,3 +10,48 @@ if (typeof window.wp.media != 'undefined') {
     window.wp.media.view.l10n.trashSelected = window.wp.media.view.l10n.trash;
     window.wp.media.view.l10n.deleteSelected = window.wp.media.view.l10n.deletePermanently;
 }
+
+if ($('.media-frame-toolbar').length == 0) {
+    setTimeout(function() {
+        $('<button type="button" class="button media-button button-primary edit-attachments-tag">Appliquer un tag</button>').insertAfter('.media-toolbar .media-toolbar-secondary .button-primary');
+
+        // Show popup
+        var popup = '<div class="add-medias-tag hidden"><ul class="themes">Thématiques</ul><ul class="places">Lieux</ul><ul class="seasons">Saisons</ul><button class="button button-primary apply">Appliquer un tag</button><button class=" button close">Annuler</button></div>';
+        $('#wpbody-content').append(popup);
+
+        $('.add-medias-tag .close').click(function(){
+            $('.add-medias-tag').addClass('hidden');
+        });
+
+        $('.add-medias-tag .apply').click(function(){
+            console.log($('.add-medias-tag input:checked'));
+        });
+
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            url: ajaxurl,
+            data: {
+                action: 'get_all_tags',
+            },
+            success: function(data) {
+                data.themes.forEach(function(element) {
+                    $('.add-medias-tag .themes').append('<li> <input type="checkbox" value="' + element.id + '"> ' + element.name + '</li>');
+                });
+
+                data.places.forEach(function(element) {
+                    $('.add-medias-tag .places').append('<li> <input type="checkbox" value="' + element.id + '">' + element.name + '</li>');
+                });
+
+                data.seasons.forEach(function(element) {
+                    $('.add-medias-tag .seasons').append('<li> <input type="checkbox" value="' + element.id + '">' + element.name + '</li>');
+                });
+            }
+        });
+
+        $('.edit-attachments-tag').click(function() {
+            // display form
+            $('.add-medias-tag').removeClass('hidden');
+        });
+    }, 3000);
+}
