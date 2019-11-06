6ee75a78530b65ddc73209c3c8099432970b7a48
Thomas Navarro <36193187+ThomasNavarro@users.noreply.github.com>
2019-11-06 08:40:37 +0100

    Update library/classes/acf/class-acf.php
    Co-Authored-By: Benoit Bouchaud <36764993+benbchd@users.noreply.github.com>

--- library/classes/acf/class-acf.php
+++ library/classes/acf/class-acf.php
@@ -433,7 +433,7 @@ class WoodyTheme_ACF
             // On retire l'option bloc commentaires si le plugin n'est pas activé
             unset($field['layouts']['layout_5d91d7a234ca6']);
         }
-        if (!in_array('neige', WOODY_OPTIONS)) {
+        if (!in_array('ski_resort', WOODY_OPTIONS)) {
             // On retire l'option bloc infolive si le plugin n'est pas activé
             unset($field['layouts']['layout_5da9bdc17563e']);
         }
