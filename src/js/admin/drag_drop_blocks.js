dfa6ab23e5dcc5a6d106d02caf68bbf59789661b
LEGENDRE <jeremy.legendre@raccourci.fr>
2019-11-07 18:06:11 +0100

    update input values on drop

--- src/js/admin/drag_drop_blocks.js
+++ src/js/admin/drag_drop_blocks.js
@@ -14,7 +14,7 @@ $('#acf-group_5afd260eeb4ab .acf-field[data-name="section"] .acf-table tbody')
                     var rowPosY = row.offset().top;
                     var maxHeight = row.innerHeight();
 
-                    if (blockPosY < rowPosY + 25 || blockPosY + 35  > rowPosY + maxHeight) {
+                    if (blockPosY < rowPosY + 25 || blockPosY + 35 > rowPosY + maxHeight) {
                         // Outside
                         block.css('z-index', '10000');
                     } else {
@@ -29,7 +29,7 @@ $('#acf-group_5afd260eeb4ab .acf-field[data-name="section"] .acf-table tbody')
 
                     if (blockPosY < rowPosY + 35 || blockPosY + 35 > rowPosY + maxHeight) {
                         // Outside
-                        updateSection(block, blockPosY);
+                        updateSection(block, blockPosY, row);
 
                     } else {
                         // Inside
@@ -39,30 +39,40 @@ $('#acf-group_5afd260eeb4ab .acf-field[data-name="section"] .acf-table tbody')
         });
     });
 
-var updateSection = function(block, blockPosY){
+var updateSection = function(block, blockPosY, original_row) {
     $('#acf-group_5afd260eeb4ab .acf-field[data-name="section"] .acf-table tbody')
         .first()
         .children('.acf-row:not(.acf-clone)')
         .each(function() {
             var row = $(this);
-            var rowPosY = row.offset().top;
-            var maxHeight = row.innerHeight();
 
+            if (row != original_row) {
+                var rowPosY = row.offset().top;
+                var maxHeight = row.innerHeight();
 
-            if (blockPosY > rowPosY && blockPosY < (rowPosY + maxHeight)) {
-                var row_id = row.data('id');
-                var layout_id = row.find('.values .layout').length;
-                var clone = block;
-                clone.removeAttr('style')
-                    .attr('data-id', layout_id)
-                    .find('input')
-                    .first()
-                    .attr('name', 'acf[field_5afd2c6916ecb][' + row_id + '][field_5b043f0525968]['+layout_id+'][acf_fc_layout]');
-                clone.find('.acf-fc-layout-handle span').text(layout_id+1);
+                if (blockPosY > rowPosY && blockPosY < (rowPosY + maxHeight)) {
+                    var row_id = row.data('id');
+                    var layout_id = row.find('.values .layout').length;
+                    var clone = block;
+                    clone.removeAttr('style')
+                        .attr('data-id', layout_id)
+                        .find('input')
+                        .first()
+                        .attr('name', 'acf[field_5afd2c6916ecb][' + row_id + '][field_5b043f0525968][' + layout_id + '][acf_fc_layout]');
+                    clone.find('.acf-fc-layout-handle span').text(layout_id + 1);
 
-                block.remove();
-                row.find('.values')
-                    .append(clone);
+                    block.remove();
+                    row.find('.values')
+                        .append(clone);
+
+                    if(row.find('.no-value-message').css("display") != "none"){
+                        row.find('.no-value-message').css("display", 'none');
+                    }
+
+                    if (original_row.find('.values .layout').length < 1) {
+                        original_row.find('.no-value-message').css("display", 'block');
+                    }
+                }
             }
-    });
+        });
 }
