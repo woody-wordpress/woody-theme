d6d5ca525af06aba4389c8680956bdf57e2939d4
LEGENDRE <jeremy.legendre@raccourci.fr>
2019-11-13 11:31:47 +0100

    div showing droppable position

--- src/js/admin/drag_drop_blocks.js
+++ src/js/admin/drag_drop_blocks.js
@@ -15,6 +15,7 @@ var clickDragButton = function(block, row) {
 
             if (blockPosY < rowPosY + 25 || blockPosY + 35 > rowPosY + maxHeight) {
                 // Outside
+                showDroppablePosition(block, blockPosY, row);
             } else {
                 // Inside
             }
@@ -23,8 +24,8 @@ var clickDragButton = function(block, row) {
             var blockPosY = block.offset().top;
             var rowPosY = row.offset().top;
             var maxHeight = row.innerHeight();
-            // block.draggable("disabled", 1);
 
+            // Remove droppable div
             if (blockPosY < rowPosY + 35 || blockPosY + 35 > rowPosY + maxHeight) {
                 // Outside
                 updateSection(block, blockPosY, row, clone);
@@ -33,6 +34,7 @@ var clickDragButton = function(block, row) {
                 // Inside : remove draggable actions
                 $('.dragging-layout').removeClass('dragging-layout');
                 block.removeAttr('style');
+                row.find('.droppable-position').remove();
             }
         }
     });
@@ -68,6 +70,7 @@ var updateSection = function(block, blockPosY, original_row, clone) {
 
                 if (blockPosY > rowPosY && blockPosY < (rowPosY + maxHeight)) {
                     block.remove();
+                    row.find('.droppable-position').remove();
 
                     var row_id = row.data('id');
                     var layout_id = row.find('.acf-flexible-content').first().children('.values').children('.layout').last().data('id') + 1;
@@ -76,6 +79,7 @@ var updateSection = function(block, blockPosY, original_row, clone) {
                     var block_type = clone.attr('data-layout');
                     $.when(row.find('.acf-actions .acf-button[data-name="add-layout"]').trigger('click')).then(function() {
                         $.when($('.acf-tooltip.acf-fc-popup a[data-layout="' + block_type + '"]').trigger('click')).then(function() {
+                            // Replace field values
                             var fields = clone.find('.acf-fields .acf-field');
                             var index = 0;
                             var new_block = row.find('.acf-flexible-content').first().children('.values').children('.layout').last();
@@ -84,6 +88,7 @@ var updateSection = function(block, blockPosY, original_row, clone) {
                                 index++;
                             });
 
+                            // Update block indexes
                             var updated_block = row.find('.acf-flexible-content').first().children('.values').children('.layout').last();
                             updated_block.find('[for]').each(function() {
                                 var old = $(this).attr('for');
@@ -114,6 +119,7 @@ var updateSection = function(block, blockPosY, original_row, clone) {
 
                             updated_block.data('id', layout_id);
 
+                            // Re-add dragging control
                             var controls = updated_block.find('.acf-fc-layout-controls');
                             controls.prepend('<a class="acf-icon small light dashicons dashicons-move" href="#" data-name="drag-layout"></a>');
 
@@ -123,10 +129,32 @@ var updateSection = function(block, blockPosY, original_row, clone) {
 
                             if (original_row.find('.values .layout').length < 1) {
                                 original_row.find('.no-value-message').css("display", 'block');
-                                // TODO: update block numbers
                             }
                         });
                     });
+                }
+            }
+        });
+}
+
+var showDroppablePosition = function(block, blockPosY, original_row) {
+    $('#acf-group_5afd260eeb4ab .acf-field[data-name="section"] .acf-table tbody')
+        .first()
+        .children('.acf-row:not(.acf-clone)')
+        .each(function() {
+            var row = $(this);
+
+            if (row.data('id') != original_row.data('id')) {
+                var rowPosY = row.offset().top;
+                var maxHeight = row.innerHeight();
+
+                if (blockPosY > rowPosY && blockPosY < (rowPosY + maxHeight)) {
+                    if (row.find('.droppable-position').length == 0) {
+                        row.find('.acf-flexible-content').first().children('.values').append('<div class="droppable-position">Drop me here !</div>')
+                    }
+                } else {
+                    // REMOVE DROPPABLE BLOCK
+                    row.find('.droppable-position').remove();
                 }
             }
         });
