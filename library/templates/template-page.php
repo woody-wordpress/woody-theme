716a32a2b737d2258d5e840f313f0a550b2f0de6
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2019-11-21 16:50:34 +0100

    make section formating re-usable + add tourtrip block in page.twig

--- library/templates/template-page.php
+++ library/templates/template-page.php
@@ -264,6 +264,8 @@ class WoodyTheme_Template_Page extends WoodyTheme_TemplateAbstract
                 $page_hero['title'] = (!empty($page_hero['title'])) ? str_replace('-', '&#8209', $page_hero['title']) : '';
                 $this->context['page_hero'] = Timber::compile($this->context['woody_components'][$page_hero['heading_woody_tpl']], $page_hero);
             }
+
+            apply_filters('woody_page_context', $this->context);
         }
     }
 
@@ -354,55 +356,7 @@ class WoodyTheme_Template_Page extends WoodyTheme_TemplateAbstract
         $this->context['sections'] = [];
         if (!empty($this->context['timberpost'])) {
             $sections = $this->context['timberpost']->get_field('section');
-
-            if (!empty($sections) && is_array($sections)) {
-                foreach ($sections as $section_id => $section) {
-                    $the_header = '';
-                    $the_layout = '';
-
-                    if (!empty($section['icon']) || !empty($section['pretitle']) || !empty($section['title']) || !empty($section['subtitle']) || !empty($section['description'])) {
-                        $the_header = Timber::compile($this->context['woody_components']['section-section_header-tpl_01'], $section);
-                    }
-
-                    // Pour chaque bloc d'une section, on compile les données dans un template Woody
-                    // Puis on les compile dans le template de grille Woody selectionné
-                    $components = [];
-                    $components['no_padding'] = $section['scope_no_padding'];
-                    $components['alignment'] = (!empty($section['section_alignment'])) ? $section['section_alignment'] : '';
-
-                    if (!empty($section['section_content'])) {
-                        foreach ($section['section_content'] as $layout_id => $layout) {
-                            // On définit un uniqid court à utiliser dans les filtres de listes en paramètre GET
-                            // Uniqid long : section . $section_id . '_section_content' . $layout_id
-                            $layout['uniqid'] = 's' . $section_id . 'sc' . $layout_id;
-                            $layout['visual_effects'] = (!empty($layout['visual_effects'])) ? $this->tools->formatVisualEffectData($layout['visual_effects']) : '';
-                            $components['items'][] = $this->process->processWoodyLayouts($layout, $this->context);
-                        }
-
-                        if (!empty($section['section_woody_tpl'])) {
-                            $the_layout = Timber::compile($this->context['woody_components'][$section['section_woody_tpl']], $components);
-                        }
-                    }
-
-                    // On récupère les données d'affichage personnalisables
-                    $display = $this->tools->getDisplayOptions($section);
-
-                    // On ajoute les 3 parties compilées d'une section + ses paramètres d'affichage
-                    // puis on compile le tout dans le template de section Woody
-                    $the_section = [
-                        'header' => $the_header,
-                        'layout' => $the_layout,
-                        'display' => $display,
-                    ];
-                    if (!empty($section['section_banner'])) {
-                        foreach ($section['section_banner'] as $banner) {
-                            $the_section[$banner] = $this->tools->getSectionBannerFiles($banner);
-                        }
-                    }
-
-                    $this->context['the_sections'][] = Timber::compile($this->context['woody_components']['section-section_full-tpl_01'], $the_section);
-                }
-            }
+            $this->context['the_sections'][] = $this->process->processWoodySections($sections, $this->context);
         }
     }
 
