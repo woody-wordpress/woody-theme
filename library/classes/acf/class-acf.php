6b67a5d8d180b2a92c1b54a8ea8667ae9d0d5623
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2019-11-21 08:47:45 +0100

    display trip infos on tourtrip

--- library/classes/acf/class-acf.php
+++ library/classes/acf/class-acf.php
@@ -175,9 +175,9 @@ class WoodyTheme_ACF
 
             switch ($field['key']) {
                 case 'field_5afd2c9616ecd': // Cas des sections
-                    $components = WoodyLibrary::getTemplatesByAcfGroup($woodyComponents, $field['key']);
-                    break;
                 case 'field_5d16118093cc1': // Cas des mises en avant de composants de séjours
+                case 'field_5dd563227f5e3': // Cas des étapes de circuit
+                    $components = WoodyLibrary::getTemplatesByAcfGroup($woodyComponents, $field['key']);
                     $components = WoodyLibrary::getTemplatesByAcfGroup($woodyComponents, $field['key']);
                     break;
                 default:
