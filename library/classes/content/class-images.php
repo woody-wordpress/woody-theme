bdfb6a33e943f74aa42306ea1691572779ccc61b
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2019-12-12 15:41:58 +0100

    hotfix => define sizes on image download

--- library/classes/content/class-images.php
+++ library/classes/content/class-images.php
@@ -187,7 +187,7 @@ class WoodyTheme_Images
                     $term = get_term($term_id);
 
                     if (!is_wp_error($term)) {
-                        wp_set_post_terms($attach_id, $term->term_id , $term->taxonomy, true);
+                        wp_set_post_terms($attach_id, $term->term_id, $term->taxonomy, true);
                     }
                 }
             }
@@ -425,11 +425,9 @@ class WoodyTheme_Images
     public function generateAttachmentMetadata($metadata, $attachment_id)
     {
         if (wp_attachment_is_image($attachment_id)) {
-            $attachment_metadata = wp_get_attachment_metadata($attachment_id);
 
-            if (!empty($attachment_metadata)) {
-                $metadata = $attachment_metadata;
-            } else {
+            if (empty($metadata['sizes'])) {
+
                 // Get current post
                 $post = get_post($attachment_id);
 
