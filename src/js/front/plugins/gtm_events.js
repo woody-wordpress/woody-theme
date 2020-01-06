9aa1c91dd7cdd5964737cfcdc70d335834b51756
LEGENDRE <jeremy.legendre@raccourci.fr>
2020-01-06 09:52:12 +0100

    event page viewed

--- src/js/front/plugins/gtm_events.js
+++ src/js/front/plugins/gtm_events.js
@@ -1,24 +1,41 @@
 import $ from 'jquery';
 
 window.dataLayer = window.dataLayer || [];
-window.dataLayer.push({ langue: window.siteConfig.current_lang });
 
-$.ajax({
-    type: 'POST',
-    dataType: 'json',
-    url: frontendajax.ajaxurl,
-    data: {
-        action: 'get_current_place',
-        post_id: globals.post_id
-    },
-    success: function(response) {
-        if (response.length > 0) {
-            window.dataLayer.push({ lieu: response[0] });
-        }
-    },
-    error: function(err) {
-        console.error(err);
+var obj = {
+    eventCategory: 'Pages',
+    eventAction: 'Page vue',
+    eventLabel: globals.post_title + '|' + globals.post_id,
+    eventValue: '',
+    page: {
+        name: globals.post_title,
+        langue: window.siteConfig.current_lang,
+        lieu: ''
     }
+};
+
+var getPlace = function() {
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        url: frontendajax.ajaxurl,
+        data: {
+            action: 'get_current_place',
+            post_id: globals.post_id
+        },
+        success: function(response) {
+            if (response.length > 0) {
+                obj.lieu =  response[0];
+            }
+        },
+        error: function(err) {
+            console.error(err);
+        }
+    });
+}
+
+$.when(getPlace()).then( function() {
+    window.dataLayer.push(obj);
 });
 
 if ($('.sharing-links').length) {
@@ -47,6 +64,17 @@ if ($('.sharing-links').length) {
 }
 
 $('.woody-component-claims-block .claim-content .claim-link .button').click(function() {
+    // TODO: voir comment réecrire l'event GTM concernant les blocs de pub
+    // var obj = {
+    //     eventCategory: 'Pages',
+    //     eventAction: 'Clic sur bloc de publicité',
+    //     eventLabel: 'Clic claim button',
+    //     eventValue: '',
+    //     page: {
+    //         name: globals.post_title
+    //     }
+    // };
+
     window.dataLayer.push({
         event: 'Clic claim button',
     });
