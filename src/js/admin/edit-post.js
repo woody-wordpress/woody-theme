cf12b92d5c63b578f43101e1631e6b9a6186ab32
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2020-01-09 16:34:32 +0100

    updated acf field

--- src/js/admin/edit-post.js
+++ src/js/admin/edit-post.js
@@ -45,8 +45,8 @@ $('#post').each(function() {
         }
     });
 
-    // On ferme certaines metaboxes ACF => Visuel et accroche, En-tête, Bloc de résa, diaporama, révisions, boxes en sidebar (sauf publier)
-    $('#acf-group_5b052bbee40a4, #acf-group_5b2bbb46507bf, #acf-group_5c0e4121ee3ed, #acf-group_5bb325e8b6b43, #revisionsdiv, #side-sortables .postbox:not(#submitdiv)').addClass('closed');
+    // On ferme certaines metaboxes ACF => Visuel et accroche, En-tête, Bloc de résa, diaporama, révisions, boxes en sidebar (sauf publier), WoodySeo
+    $('#acf-group_5b052bbee40a4, #acf-group_5b2bbb46507bf, #acf-group_5c0e4121ee3ed, #acf-group_5bb325e8b6b43, #revisionsdiv, #side-sortables .postbox:not(#submitdiv), #acf-group_5d7f7cd5615c0').addClass('closed');
 
     var countElements = function(field) {
         var $parent = field.parent().$el;
