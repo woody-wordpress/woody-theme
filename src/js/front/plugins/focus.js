48b9221b194c7274588539d9a548c1cd5303f5c6
Thomas Navarro <thomas.navarro@raccourci.fr>
2020-02-28 16:05:13 +0100

    fix

--- src/js/front/plugins/focus.js
+++ src/js/front/plugins/focus.js
@@ -15,11 +15,7 @@
 
             // Mouse events
             body.addEventListener('mousedown', this.preFocus);
-
-            // Touch events
-            body.addEventListener('touchstart', this.addFocus);
-            body.addEventListener('touchend', this.removeFocus);
-        };
+        }
 
         preFocus(event) {
             this.usingMouse = (event.type === 'mousedown');
@@ -29,11 +25,11 @@
             if (this.usingMouse) {
                 event.target.classList.add('focus-mouse');
             }
-        };
+        }
 
         removeFocus(event) {
             event.target.classList.remove('focus-mouse');
-        };
+        }
     }
 
     const trackFocus = new Focus();
