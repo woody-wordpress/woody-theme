6a85a79f14f3d98cfa740f49f0eeb54acd02291b
LEGENDRE <jeremy.legendre@raccourci.fr>
2020-03-02 16:35:15 +0100

    Page teaser media OR profile bugfix

--- src/js/admin/edit-post.js
+++ src/js/admin/edit-post.js
@@ -356,4 +356,11 @@ $('#post').each(function() {
         });
     })
 
+    $('.acf-field-5b44bfc2e2e21 .acf-switch:not(-on)').on('click', function() {
+        $('.acf-field-5c614d83a4e9b .acf-switch.-on').trigger('click');
+    });
+
+    $('.acf-field-5c614d83a4e9b .acf-switch:not(-on)').on('click', function() {
+        $('.acf-field-5b44bfc2e2e21 .acf-switch.-on').trigger('click');
+    });
 });
