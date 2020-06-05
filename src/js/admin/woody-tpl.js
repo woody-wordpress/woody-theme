9b0b5cee1e70586eff1a20af65ff43a33557b4a1
LEGENDRE <jeremy.legendre@raccourci.fr>
2020-06-05 16:21:17 +0200

    up

--- src/js/admin/woody-tpl.js
+++ src/js/admin/woody-tpl.js
@@ -1,24 +1,46 @@
 import $ from 'jquery';
 
 $('#post').each(function() {
-    $('#post-body-content').append('<div id="tpls_popin" class="hidden"><ul></ul></div>');
+    const accepted_post = ['page', 'claims', 'woody_rdbk_leaflets'];
+
+    if (accepted_post.includes($('#post_type').val())) {
+        $('#post-body-content').append('<div id="tpls_popin" class="hidden"><a href="#" class="close"></a><a href="#" class="save"></a><ul></ul></div>');
+        $('#tpls_popin .close').on('click', function() {
+            $('#tpls_popin').addClass('hidden');
+            $('#tpls_popin').removeClass('opened');
+        });
+
+    }
 
    $(document).ready( function() {
         // AJAX to get all woody_tpl
 
-        $.ajax({
-            type: 'POST',
-            dataType: 'json',
-            url: ajaxurl,
-            data: {
-                action: 'woody_tpls',
-            },
-            success: function(data) {
-                for (let [key, value] of Object.entries(data)) {
-                    $('#tpls_popin ul').append('<li>' + value + '</li>');
-                }
-            },
-            error: function() {},
+        $('#tpls_popin').each(function() {
+            $.ajax({
+                type: 'POST',
+                dataType: 'json',
+                url: ajaxurl,
+                data: {
+                    action: 'woody_tpls',
+                },
+                success: function(data) {
+                    for (let [key, value] of Object.entries(data)) {
+                        $('#tpls_popin ul').append('<li>' + value + '</li>');
+                    }
+
+
+
+                    $('.woody-tpl-button').on('click', function() {
+                        $this = $(this);
+
+                        $('#tpls_popin').removeClass('hidden');
+                        $('#tpls_popin').addClass('opened');
+
+
+                    });
+                },
+                error: function() {},
+            });
         });
     });
 });
