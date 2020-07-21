5238a2db3b5e29e65b6820e59014ea73edbb177e
Léo POIROUX <leo.poiroux@gmail.com>
2020-07-21 09:09:44 +0200

    Version 1.28.4

--- library/functions/04_custom_appearance.php
+++ library/functions/04_custom_appearance.php
@@ -2,7 +2,7 @@
 
 function getActiveShares()
 {
-    $return['current_url'] = add_query_arg([$_GET], get_permalink());
+    $current_url = add_query_arg([$_GET], get_permalink());
     $return['current_media'] = !empty(get_field('field_5b0e5ddfd4b1b')) ? get_field('field_5b0e5ddfd4b1b')['url'] : "";
     $active_shares = get_field('field_5ee9c784e017d', 'option');
     $path_icons = get_template_directory() . '/src/icons/shares/';
@@ -10,27 +10,27 @@ function getActiveShares()
     $shares = [
         'facebook' => [
             'sharing_message' => __('Partager sur Facebook', 'woody-theme'),
-            'sharing_link' => 'https://facebook.com/sharer/sharer.php?u',
+            'sharing_link' => 'https://facebook.com/sharer/sharer.php?u=' . $current_url,
             'sharing_icon' => file_get_contents($path_icons . 'facebook.svg')
         ],
         'twitter' => [
             'sharing_message' => __('Partager sur Twitter', 'woody-theme'),
-            'sharing_link' => 'https://twitter.com/intent/tweet/?url',
+            'sharing_link' => 'https://twitter.com/intent/tweet/?url=' . $current_url,
             'sharing_icon' => file_get_contents($path_icons . 'twitter.svg')
         ],
         'whatsapp' => [
             'sharing_message' => __('Partager sur WhatsApp', 'woody-theme'),
-            'sharing_link'=> 'whatsapp://send?text',
+            'sharing_link'=> 'whatsapp://send?text=' . $current_url,
             'sharing_icon' => file_get_contents($path_icons . 'whatsapp.svg')
         ],
         'pinterest' => [
             'sharing_message' => __('Partager sur Pinterest', 'woody-theme'),
-            'sharing_link' => 'https://pinterest.com/pin/create/button/?url',
+            'sharing_link' => 'https://pinterest.com/pin/create/button/?url=' . $current_url,
             'sharing_icon' => file_get_contents($path_icons . 'pinterest.svg')
         ],
         'mail' => [
             'sharing_message' => __('Partager par e-mail', 'woody-theme'),
-            'sharing_link' => 'mailto:?body',
+            'sharing_link' => 'mailto:?body=' . $current_url,
             'sharing_icon' => file_get_contents($path_icons . 'mail.svg')
         ]
     ];
