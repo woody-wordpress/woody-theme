55b90190d01b68250915ae51a50c5d3aa6012f1b
Léo POIROUX <leo.poiroux@gmail.com>
2020-07-30 09:29:39 +0200

    bugfix admin

--- library/classes/assets/class-enqueue-assets.php
+++ library/classes/assets/class-enqueue-assets.php
@@ -313,20 +313,19 @@ class WoodyTheme_Enqueue_Assets
         // Define $this->isTouristicPlaylist, $this->isTouristicSheet et $this->wThemeVersion
         $this->setGlobalVars();
 
-        wp_enqueue_script('jsdelivr_lazysizes', 'https://cdn.jsdelivr.net/npm/lazysizes@4.1.2/lazysizes.min.js', [], null);
-
         // Enqueue the main Scripts
-        $dependencies = ['jquery'];
-        wp_enqueue_script('admin-javascripts', $this->assetPath(WP_DIST_URL . '/js/admin.js'), $dependencies, null);
-        wp_enqueue_script('admin_jsdelivr_flatpickr', 'https://cdn.jsdelivr.net/npm/flatpickr@4.5.7/dist/flatpickr.min.js', [], null);
-        wp_enqueue_script('admin_jsdelivr_flatpickr_l10n', 'https://cdn.jsdelivr.net/npm/flatpickr@4.5.7/dist/l10n/fr.min.js', ['admin_jsdelivr_flatpickr'], null);
+        $dependencies = ['jquery', 'admin-jsdelivr-lazysizes', 'admin_jsdelivr_flatpickr', 'admin_jsdelivr_flatpickr_l10n'];
+        wp_enqueue_script('admin-jsdelivr-lazysizes', 'https://cdn.jsdelivr.net/npm/lazysizes@4.1.2/lazysizes.min.js', [], null, true);
+        wp_enqueue_script('admin-javascripts', $this->assetPath(WP_DIST_URL . '/js/admin.js'), $dependencies, $this->wThemeVersion, true);
+        wp_enqueue_script('admin_jsdelivr_flatpickr', 'https://cdn.jsdelivr.net/npm/flatpickr@4.5.7/dist/flatpickr.min.js', [], null, true);
+        wp_enqueue_script('admin_jsdelivr_flatpickr_l10n', 'https://cdn.jsdelivr.net/npm/flatpickr@4.5.7/dist/l10n/fr.min.js', ['admin_jsdelivr_flatpickr'], null, true);
 
         // Added global vars
         wp_add_inline_script('admin-javascripts', 'var siteConfig = ' . json_encode($this->siteConfig) . ';', 'before');
         wp_add_inline_script('admin-javascripts', 'document.addEventListener("DOMContentLoaded",()=>{document.body.classList.add("windowReady")});', 'after');
 
         // Enqueue the main Stylesheet.
-        wp_enqueue_style('admin-stylesheet', $this->assetPath(WP_DIST_URL . '/css/admin.css'), [], null);
+        wp_enqueue_style('admin-stylesheet', $this->assetPath(WP_DIST_URL . '/css/admin.css'), [], $this->wThemeVersion);
     }
 
     public function heartbeatSettings()
