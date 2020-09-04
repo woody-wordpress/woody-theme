46c53c13b8759cdb58f3150ebf0d9243448812fc
Léo POIROUX <leo.poiroux@gmail.com>
2020-09-04 09:11:03 +0200

    rename filter woody_seo_edit_meta_string

--- library/templates/template.php
+++ library/templates/template.php
@@ -126,7 +126,7 @@ abstract class WoodyTheme_TemplateAbstract
 
         // SEO Context
         $this->context['title'] = (!empty(get_field('field_5d7f7dea20bb1'))) ? woody_untokenize(get_field('woodyseo_meta_title')) : get_the_title() . ' | ' . $this->context['site']['name'];
-        $this->context['title'] = apply_filters('woody_seo_edit_meta_string', $this->context['title']);
+        $this->context['title'] = apply_filters('woody_seo_transform_pattern', $this->context['title']);
         $this->context['metas'] = $this->setMetadata();
         $this->context['custom_meta'] = get_field('woody_custom_meta', 'options');
 
@@ -387,7 +387,7 @@ abstract class WoodyTheme_TemplateAbstract
             foreach ($woody_seo_data as $data_key => $data) {
                 if (is_string($data)) {
                     $woody_seo_data[$data_key] = trim($data);
-                    $data = apply_filters('woody_seo_edit_meta_string', $data);
+                    $data = apply_filters('woody_seo_transform_pattern', $data);
                 }
 
                 switch ($data_key) {
