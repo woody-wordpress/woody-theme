9da787f29397f1eeeb648bae7ec0f491f59af281
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2020-09-09 12:06:55 +0200

    fix sheet meta title

--- library/templates/template.php
+++ library/templates/template.php
@@ -125,7 +125,7 @@ abstract class WoodyTheme_TemplateAbstract
         $this->context['woody_access_staging'] = WOODY_ACCESS_STAGING;
 
         // SEO Context
-        $this->context['title'] = (!empty(get_field('woodyseo_meta_title'))) ? woody_untokenize(get_field('woodyseo_meta_title')) : get_the_title() . ' | ' . $this->context['site']['name'];
+        $this->context['title'] = (!empty(get_field('field_5d7f7dea20bb1'))) ? woody_untokenize(get_field('woodyseo_meta_title')) : get_the_title() . ' | ' . $this->context['site']['name'];
         $this->context['title'] = apply_filters('woody_seo_transform_pattern', $this->context['title']);
         $this->context['metas'] = $this->setMetadata();
         $this->context['custom_meta'] = get_field('woody_custom_meta', 'options');
