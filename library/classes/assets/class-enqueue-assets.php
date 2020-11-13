2ad159aa6a54f43f9c1d0546dc32c28c7b793c78
Léo POIROUX <leo.poiroux@gmail.com>
2020-11-13 15:12:40 +0100

    Disable Addons Cookies CSS

--- library/classes/assets/class-enqueue-assets.php
+++ library/classes/assets/class-enqueue-assets.php
@@ -151,9 +151,6 @@ class WoodyTheme_Enqueue_Assets
         //     wp_enqueue_script('jsdelivr_jquery-migrate', 'https://cdn.jsdelivr.net/npm/jquery-migrate@3.0.1/dist/jquery-migrate.min.js', ['jquery'], null);
         // }
 
-        // Dependencies of main.js
-        wp_enqueue_script('jsdelivr_cookieconsent', 'https://cdn.jsdelivr.net/npm/cookieconsent@3.1.0/build/cookieconsent.min.js', [], null);
-
         if (!$this->isTouristicSheet || $this->isRoadBookSheet) {
             wp_enqueue_script('jsdelivr_swiper', 'https://cdn.jsdelivr.net/npm/swiper@4.4.1/dist/js/swiper.min.js', [], null);
         }
