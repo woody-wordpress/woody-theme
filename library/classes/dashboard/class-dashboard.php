7d4da272654b8ad3d9e0279d6d563a734fd9e285
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2020-12-31 14:30:47 +0100

    Update RC Family members

--- library/classes/dashboard/class-dashboard.php
+++ library/classes/dashboard/class-dashboard.php
@@ -48,7 +48,7 @@ class WoodyTheme_Dashboard
         $vars = [
             'class' => 'raccourci-family',
             'cover' => get_template_directory_uri() . '/library/classes/dashboard/assets/cover-raccourci-family.jpg',
-            'title' => 'Groupe Facebook · 331 membres',
+            'title' => 'Groupe Facebook · 476 membres',
             'description' => 'Un lieu d&#039;échange de partage et de bonnes idées entre tous les membres de la Raccourci Family !',
             'button_link' => 'https://www.facebook.com/plugins/group/join/popup/?group_id=355097798174987&amp;source=email_campaign_plugin',
             'button_icon' => 'facebook',
