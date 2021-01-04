ec9fc69acfe658c90d049cbf0091f8daa9142e62
Léo POIROUX <leo.poiroux@gmail.com>
2021-01-04 16:05:34 +0100

    cleaning

--- library/classes/content/class-profiles.php
+++ library/classes/content/class-profiles.php
@@ -125,9 +125,5 @@ class WoodyTheme_Profiles
             $pll_option['post_types'][] = 'profile';
             update_option('polylang', $pll_option);
         }
-
-        //TODO: function de nettoyage à supprimer quand on aura déployer tous nos sites
-        $pll_option['post_types'] = array_unique($pll_option['post_types']);
-        update_option('polylang', $pll_option);
     }
 }
