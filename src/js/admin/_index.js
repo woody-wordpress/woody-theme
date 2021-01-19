b7e2db57edd879b2ede6df178fbcfd9aad0abdd1
ColinEspinas <colin.espinas@yahoo.fr>
2021-01-19 17:05:55 +0100

    Added no reload saving when needed

--- src/js/admin/_index.js
+++ src/js/admin/_index.js
@@ -8,8 +8,10 @@ import './edit-post-close-acf';
 import './edit-post-page-teaser-media';
 // import './edit-post-sticky-actions';
 import './edit-post-unpublisher';
+import './edit-post-save-ajax';
 import './global-admin';
 import './nav-menus-ui';
 import './tags-primary';
 import './woody-seo.js';
 import './woody-tpl.js';
+
