76ac7ece496a3aaaa13445c601b7a90d1be81c93
Jérémy Legendre <50450932+JeremyLegendre@users.noreply.github.com>
2021-04-23 09:10:50 +0200

    fix

--- src/js/admin/ajax-load-layouts.js
+++ src/js/admin/ajax-load-layouts.js
@@ -46,67 +46,69 @@
             const buttons = document.querySelectorAll('.acf-tooltip.acf-fc-popup>ul>li>a');
 
             for (const button of buttons) {
-
-              let layouts = clone.find('div[data-layout="' + button.dataset.layout + '"]');
-              let none = true;
-              layouts.each(function(){
-                  if($(this).closest('[data-name="light_section_content"]').length == 0) {
-                      none = false;
-                  }
-              });
-
                 let layouts = clone.find('div[data-layout="' + button.dataset.layout + '"]');
                 let none = true;
                 layouts.each(function(){
-                    // Si le layout est enfant de light section_content alors, ça ne compte pas ce n'est pas un clone qui nous intéresse
                     if($(this).closest('[data-name="light_section_content"]').length == 0) {
                         none = false;
                     }
                 });
-
+                
                 if (layouts.length <= 0 || none) {
+                    
+                    if (!button.getAttribute('hasListener')) {
+                        button.addEventListener('click', (e) => {
 
-                    $.ajax({
-                        url: ajaxurl,
-                        data: {
-                            action: 'generate_layout_acf_clone',
-                            layout: button.dataset.layout,
-                            key: 'field_5b043f0525968'
-                        },
-                        type: 'GET',
-                        success: function(response) {
-                            response = response.replace(/name="#rowindex-name#/g, 'name="' + name);
-                            response = response.replace(/for="#rowindex-name#/g, 'for="' + id);
-                            response = response.replace(/id="#rowindex-name#/g, 'id="' + id);
-
-                            // Add clone
-                            clone.append(response);
-
-                            let fields = acf.getFields({
-                                key: 'field_5b043f0525968'
-                            });
-                            let field;
-                            fields.forEach(element => {
-                                if (element.$el[0] == clone.closest('.acf-field-flexible-content')[0]) {
-                                    field = element;
+                            let layouts = clone.find('div[data-layout="' + button.dataset.layout + '"]');
+                            let none = true;
+                            layouts.each(function(){
+                                // Si le layout est enfant de light section_content alors, ça ne compte pas ce n'est pas un clone qui nous intéresse
+                                if($(this).closest('[data-name="light_section_content"]').length == 0) {
+                                    none = false;
                                 }
                             });
 
-                            // Add Block
-                            $el = addBlock(field, {
-                                layout: button.dataset.layout,
-                            });
-                        },
-                        error: function(error) {
+                            if (layouts.length <= 0 || none) {
+                                $.ajax({
+                                    url: ajaxurl,
+                                    data: {
+                                        action: 'generate_layout_acf_clone',
+                                        layout: button.dataset.layout,
+                                        key: 'field_5b043f0525968'
+                                    },
+                                    type: 'GET',
+                                    success: function(response) {
+                                        response = response.replace(/name="#rowindex-name#/g, 'name="' + name);
+                                        response = response.replace(/for="#rowindex-name#/g, 'for="' + id);
+                                        response = response.replace(/id="#rowindex-name#/g, 'id="' + id);
 
-                          console.warn(error);
+                                        // Add clone
+                                        clone.append(response);
 
-                        }
-                    });
+                                        let fields = acf.getFields({key: 'field_5b043f0525968'});
+                                        let field;
+                                        fields.forEach(element => {
+                                            if (element.$el[0] == clone.closest('.acf-field-flexible-content')[0]) {
+                                                field = element;
+                                            }
+                                        });
 
-                    button.setAttribute('hasListener', true);
-                  }
-              }
+                                        // Add Block
+                                        $el = addBlock(field, {
+                                            layout: button.dataset.layout,
+                                        });
+                                    },
+                                    error: function(error) {
+                                        console.warn(error);
+                                    }
+                                });
+                            }
+                            window.dispatchEvent(cloneEvent);
+                        });
+
+                        button.setAttribute('hasListener', true);
+                    }
+                }
             }
         });
     });
