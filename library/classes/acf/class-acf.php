6108964e6d80b1057914f5328d135d14f2b6f303
Baptiste Dayraut <baptiste.dayraut@raccourci.fr>
2021-11-22 14:15:38 +0100

    [func] clear acf setting and social / add function add in acf subfield socials

--- library/classes/acf/class-acf.php
+++ library/classes/acf/class-acf.php
@@ -73,6 +73,8 @@ class WoodyTheme_ACF
 
         add_filter('acf/load_field/name=tags_primary', [$this, 'addPrimaryTagsFields'], 10, 3);
         add_filter('acf/load_field/key=field_5d91c4559736e', [$this, 'loadDisqusField'], 10, 3);
+        add_filter('acf/load_field/key=field_5ee9c784e017d', [$this, 'addSocialChoices'], 10, 3);
+        add_filter('acf/load_field/key=field_618544f164397', [$this, 'addSocialChoices'], 10, 3);
 
         // Custom Filter
         add_filter('woody_get_field_option', [$this, 'woodyGetFieldOption'], 10, 3);
@@ -999,5 +1001,21 @@ class WoodyTheme_ACF
         }
 
         wp_send_json($return);
+    }
+
+    public function addSocialChoices($field)
+    {
+        if(empty($field['choices'])){
+            $field['choices'] = array (
+                "facebook" => "Facebook",
+                "twitter" => "Twitter",
+                "linkedin" => "LinkedIn",
+                "whatsapp" => "WhatsApp",
+                "pinterest" => "Pinterest",
+                "mail" => "Mail"
+            );
+        };
+
+        return $field;
     }
 }
