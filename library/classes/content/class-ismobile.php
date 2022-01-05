15865e4d75f9f7ec55064e21e4ca91ff0b6f16e7
Léo POIROUX <leo.poiroux@gmail.com>
2022-01-05 16:47:27 +0100

    x-ua-device

--- library/classes/content/class-ismobile.php
+++ library/classes/content/class-ismobile.php
@@ -21,8 +21,14 @@ class WoodyTheme_IsMobile
 
     public function wp_is_mobile($is_mobile)
     {
-        if (!empty($_SERVER['X-UA-Device'])) {
-            if ($_SERVER['X-UA-Device'] == 'mobile') {
+        $headers = getallheaders();
+        foreach ($headers as $key => $val) {
+            unset($headers[$key]);
+            $headers[strtolower($key)] = $val;
+        }
+
+        if (!empty($headers['x-ua-device'])) {
+            if ($headers['x-ua-device'] == 'mobile') {
                 return true;
             } else {
                 return false;
