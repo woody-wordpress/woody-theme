ed88089176e2217013221ddbca4b405cb191f6f0
Léo POIROUX <leo.poiroux@gmail.com>
2022-03-17 11:02:02 +0100

    duplicate_post_types_enabled

--- library/classes/plugins/class-plugins-options.php
+++ library/classes/plugins/class-plugins-options.php
@@ -344,13 +344,15 @@ class WoodyTheme_Plugins_Options
         ];
         $this->updateOption('redirection_options', $redirection_options);
 
-
         // Duplicate Post
         $duplicate_post_types_enabled = [
             'post',
             'page',
             'profile',
             'short_link',
+            'testimony',
+            'woody_model',
+            'woody_section_model',
             'woody_claims',
         ];
         $this->updateOption('duplicate_post_types_enabled', $duplicate_post_types_enabled);
