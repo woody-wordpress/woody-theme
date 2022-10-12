80a1cc2dc0c12c4a6538b7dce5f8486934f2aca3
Orphée Besson <orphee.besson@raccourci.fr>
2022-10-12 09:24:17 +0200

    [WIP] class-unpublisher

--- library/classes/content/class-unpublisher.php
+++ library/classes/content/class-unpublisher.php
@@ -18,10 +18,10 @@ class WoodyTheme_Unpublisher
     protected function registerHooks()
     {
         add_action('add_meta_boxes', array($this, 'registerMetaBox'));
-        // add_action('save_post', array($this, 'saveUnpublisherParams'));
+        add_action('save_post', array($this, 'saveUnpublisherParams'));
 
         add_action('woody_theme_update', [$this, 'scheduleUnpublishPosts']);
-        // add_action('woody_unpublish_posts', [$this, 'woodyUnpublishPosts']);
+        add_action('woody_unpublish_posts', [$this, 'woodyUnpublishPosts']);
     }
 
     public function registerMetaBox()
@@ -29,6 +29,7 @@ class WoodyTheme_Unpublisher
         $post_types = get_post_types(['public' => true, '_builtin' => false]);
         $post_types['page'] = 'page';
 
+        // TODO: add apply filters
         $exclude = ['short_link', 'snowflake_config', 'testimony', 'touristic_sheet', 'woody_model', 'woody_section_model'];
 
         foreach ($post_types as $post_type) {
@@ -60,54 +61,55 @@ class WoodyTheme_Unpublisher
         echo '<div><small><i>À compter de la date choisie (+/- 1h), le contenu passe en brouillon</div></small></i>';
     }
 
-    // public function saveUnpublisherParams($post_id)
-    // {
-    //     if (!isset($_POST['saveUnpublisherParams_nonce'])) {
-    //         return $post_id;
-    //     }
+    public function saveUnpublisherParams($post_id)
+    {
+        if (!isset($_POST['saveUnpublisherParams_nonce'])) {
+            return $post_id;
+        }
 
-    //     if (!wp_verify_nonce($_POST['saveUnpublisherParams_nonce'], 'saveUnpublisherParams')) {
-    //         return $post_id;
-    //     }
+        if (!wp_verify_nonce($_POST['saveUnpublisherParams_nonce'], 'saveUnpublisherParams')) {
+            return $post_id;
+        }
 
-    //     update_post_meta($post_id, '_wUnpublisher_date', $_POST['wUnpublisher_date']);
-    // }
+        update_post_meta($post_id, '_wUnpublisher_date', $_POST['wUnpublisher_date']);
+    }
 
     public function scheduleUnpublishPosts()
     {
-        // if (!wp_next_scheduled('woody_unpublish_posts')) {
-        //     wp_schedule_event(time(), 'hourly', 'woody_unpublish_posts');
-        //     output_success(sprintf('+ Schedule %s', 'woody_unpublish_posts'));
-        // }
-
-        if (wp_next_scheduled('woody_unpublish_posts')) {
-            wp_clear_scheduled_hook('woody_unpublish_posts');
+        if (!wp_next_scheduled('woody_unpublish_posts')) {
+            wp_schedule_event(time(), 'hourly', 'woody_unpublish_posts');
             output_success(sprintf('- Schedule %s', 'woody_unpublish_posts'));
         }
     }
 
-    // public function woodyUnpublishPosts()
-    // {
-    //     global $wpdb;
+    public function woodyUnpublishPosts()
+    {
+        global $wpdb;
 
-    //     $today = date(DATE_ATOM);
-    //     $results = $wpdb->get_results(
-    //         "SELECT `wp_posts`.`ID`
-    //         FROM `wp_posts`, `wp_postmeta`
-    //         WHERE `wp_posts`.`ID` = `wp_postmeta`.`post_id`
-    //         AND `wp_postmeta`.`meta_key` = '_wUnpublisher_date'
-    //         AND `wp_postmeta`.`meta_value` < {$today}
-    //         AND `wp_postmeta`.`meta_value` != ''"
-    //     );
+        // TODO: use WOODY_TIMEZONE to get the current date based on the right time zone
+        $today = date(DATE_ATOM);
 
-    //     if (!empty($results)) {
-    //         foreach ($results as $result) {
-    //             wp_update_post([
-    //                 'ID' => $result->ID,
-    //                 'post_status' => 'draft'
-    //             ]);
-    //             update_post_meta($result->ID, '_wUnpublisher_date', '');
-    //         }
-    //     }
-    // }
+        // On récupère les posts publiés qui ont une date de dépublication inférieure à la date courante
+        // TODO: use {$wpdb->prefix}
+        $results = $wpdb->get_results(
+        "SELECT wp_posts.ID
+        FROM wp_posts, wp_postmeta
+        WHERE wp_posts.ID = wp_postmeta.post_ID
+        AND wp_posts.post_status = 'publish'
+        AND wp_postmeta.meta_key = '_wUnpublisher_date'
+        AND wp_postmeta.meta_value != ''
+        AND wp_postmeta.meta_value < '{$today}'
+        ");
+
+        if (!empty($results)) {
+            foreach ($results as $result) {
+                wp_update_post([
+                    'ID' => $result->ID,
+                    'post_status' => 'draft'
+                ]);
+                update_post_meta($result->ID, '_wUnpublisher_date', '');
+                clean_post_cache($result->ID);
+            }
+        }
+    }
 }
