49bbac0c46863f3203f8474ca720b57abad7a870
Orphée Besson <orphee.besson@raccourci.fr>
2022-10-20 14:36:45 +0200

    [WIP] continue thumbnail preview (some style & add endpoint)

--- library/classes/content/class-api-rest.php
+++ library/classes/content/class-api-rest.php
@@ -22,6 +22,13 @@ class WoodyTheme_Api_Rest
                 'callback' => [$this, 'getPagePreviewApiRest']
             ));
         });
+
+        add_action('rest_api_init', function () {
+            register_rest_route('woody', 'components/templates', array(
+                'methods' => 'GET',
+                'callback' => [$this, 'getWoodyTemplatesConfig']
+            ));
+        });
     }
 
     public function getPagePreviewApiRest() {
@@ -46,5 +53,37 @@ class WoodyTheme_Api_Rest
 
             return getPagePreview($wrapper, $post);
         }
+    }
+
+    public function getWoodyTemplatesConfig()
+    {
+        $return = [];
+
+        $woodyComponents = getWoodyComponents();
+
+        foreach ($woodyComponents as $key => $component) {
+            if(empty($component['thumbnails']['small'])) {
+                $thumbnail = '';
+            } else {
+                if (strpos($component['thumbnails']['small'], 'custom_woody_tpls') === false) {
+                    $img_views_path = '/img/woody-library/views/';
+                } else {
+                    $img_views_path = apply_filters('custom_woody_tpls_thumbnails_path', '/img/', $component['thumbnails']['small']);
+                }
+
+                $thumbnail = WP_HOME . '/app/dist/' . WP_SITE_KEY . $img_views_path . $component['thumbnails']['small'] . '?version=' . get_option('woody_theme_version');
+            }
+
+            $return[$key] = [
+                'name' => empty($component['name']) ? '' : $component['name'],
+                'description' => empty($component['description']) ? '' : $component['description'],
+                'thumbnail' => $thumbnail,
+                'display_options' => empty($component['display']) ? '' : $component['display'],
+                'is_new_tpl' => empty($component['creation']) ? false : isWoodyNewTpl($component['creation']),
+                'acf_groups' => empty($component['acf_groups']) ? '' : $component['acf_groups']
+            ];
+        }
+
+        return $return;
     }
 }
