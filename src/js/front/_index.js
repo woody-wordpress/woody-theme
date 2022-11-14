6cca073772293dbd91540dcb3e244d7f9db82dbd
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2022-11-14 15:46:08 +0100

    [test] PWA service worker

--- src/js/front/_index.js
+++ src/js/front/_index.js
@@ -1,3 +1,8 @@
+// Register SW
+if (window.location.origin.includes('sandbox') && 'serviceWorker' in navigator) {
+    navigator.serviceWorker.register(window.location.origin + '/app/themes/woody-theme/pwa/woody-sw.js');
+}
+
 import 'woody-library/assets/js/_index';
 import './framework/foundation-explicit-pieces';
 import './plugins/scroll_to_top';
