92ac8f8e07d26467d1ff0cd8b911dfee34373910
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2022-11-14 16:05:22 +0100

    [vers] 1.68.3 PWA sw scope

--- src/js/front/_index.js
+++ src/js/front/_index.js
@@ -1,6 +1,6 @@
 // Register SW
 if (window.location.origin.includes('sandbox') && 'serviceWorker' in navigator) {
-    navigator.serviceWorker.register(window.location.origin + '/app/themes/woody-theme/pwa/woody-sw.js');
+    navigator.serviceWorker.register(window.location.origin + '/app/themes/woody-theme/pwa/woody-sw.js', {'scope' : '/'});
 }
 
 import 'woody-library/assets/js/_index';
