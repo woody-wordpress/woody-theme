f40910dc2f2bd1139fccf27508ef92526f75b7f9
Valentin Laurent <valentin.laurent@raccourci.fr>
2022-11-28 16:18:56 +0100

    Add Listenner and condition

--- pwa/sw-register.js
+++ pwa/sw-register.js
@@ -4,5 +4,31 @@ if ('serviceWorker' in navigator) {
         console.log('Registration succeeded. Scope is ' + reg.scope);
     }).catch(function(error) {
         console.log('Registration failed with ' + error);
-      });
+    });
+}
+
+const isPWA = ['fullscreen', 'standalone', 'minimal-ui'].some((displayMode) => window.matchMedia('(display-mode: ' + displayMode + ')').matches);
+if (!isPWA) {
+    // check if user has already refused to install PWA
+    let refused = false;
+    const cookieName = 'pwarefused';
+    document.cookie.split(';').forEach((cookie) => {
+    if (cookie.includes(cookieName)) {
+        refused = true;
+    }
+    });
+
+    if (!refused) {
+        window.addEventListener('appinstalled', () => {
+            console.log('app has been installed on desktop !');
+            // dans ce cas ne pas afficher la modal
+        });
+
+        window.addEventListener('beforeinstallprompt', (e) => {
+            e.preventDefault();
+            deferredPrompt = e;
+
+            // Afficher la modal ici
+        });
+    }
 }
