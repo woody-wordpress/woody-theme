a85b7d168c779c6bed1517ff0a21147c7c6e3b3f
Léo POIROUX <leo.poiroux@gmail.com>
2023-04-27 14:31:37 +0200

    up

--- src/js/admin/global-admin.js
+++ src/js/admin/global-admin.js
@@ -5,7 +5,7 @@ $('body.themes-php').each(function () {
 });
 
 // Bugfix  Enhanced media buttons
-if (typeof window.wp.media != 'undefined' && typeof window.wp.media.view.l10n != 'undefined') {
+if (window.wp !== undefined && window.wp.media !== undefined && window.wp.media.view !== undefined && window.wp.media.view.l10n !== undefined) {
     window.wp.media.view.l10n.cancelSelection = window.wp.media.view.l10n.cancel;
     window.wp.media.view.l10n.trashSelected = window.wp.media.view.l10n.trash;
     window.wp.media.view.l10n.deleteSelected = window.wp.media.view.l10n.deletePermanently;
