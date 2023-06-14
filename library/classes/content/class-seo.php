01503673503b45a397c68b5b64e4729e13c2a5df
thomas maillet <thomasmaillet@MacBook-Pro-de-Raccourci.local>
2023-06-14 09:03:15 +0200

    [feat] add acf field for blog name/description and touristic_sheet

--- library/classes/content/class-seo.php
+++ library/classes/content/class-seo.php
@@ -22,6 +22,7 @@ class WoodyTheme_Seo
         add_action('admin_menu', [$this, 'generateMenu'], 10);
         add_action('acf/init', [$this, 'acfAddFields']);
         add_action('members_register_caps', [$this, 'membersRegisterCaps']);
+        add_action('acf/save_post', [$this, 'test']);
     }
 
     public function woodySeoTransformPattern($string)
@@ -67,6 +68,107 @@ class WoodyTheme_Seo
 
                 acf_add_local_field($new_field);
             }
+
+
+            $woody_custom_meta_field = get_field_object('field_64887499e6961');
+
+            foreach ($languages as $lang) {
+                $new_field = $woody_custom_meta_field;
+                $new_field['key'] .= '_' . $lang;
+                $new_field['label'] .= ' (uniquement sur ' . strtoupper($lang) . ')';
+                $new_field['name'] .= '_' . $lang;
+                $new_field['_name'] .= '_' . $lang;
+                $new_field['value'] = get_field($new_field['name']);
+                $new_field['parent'] = 'group_5e16e3b48d2d6';
+
+                acf_add_local_field($new_field);
+            }
+
+            $woody_custom_meta_field = get_field_object('field_64887519e6962');
+
+            foreach ($languages as $lang) {
+                $new_field = $woody_custom_meta_field;
+                $new_field['key'] .= '_' . $lang;
+                $new_field['label'] .= ' (uniquement sur ' . strtoupper($lang) . ')';
+                $new_field['name'] .= '_' . $lang;
+                $new_field['_name'] .= '_' . $lang;
+                $new_field['value'] = get_field($new_field['name']);
+                $new_field['parent'] = 'group_5e16e3b48d2d6';
+
+                acf_add_local_field($new_field);
+            }
+
+            $woody_custom_meta_field = get_field_object('field_64887531e6963');
+
+            foreach ($languages as $lang) {
+                $new_field = $woody_custom_meta_field;
+                $new_field['key'] .= '_' . $lang;
+                $new_field['label'] .= ' (uniquement sur ' . strtoupper($lang) . ')';
+                $new_field['name'] .= '_' . $lang;
+                $new_field['_name'] .= '_' . $lang;
+                $new_field['value'] = get_field($new_field['name']);
+                $new_field['parent'] = 'group_5e16e3b48d2d6';
+
+                acf_add_local_field($new_field);
+            }
         }
     }
+
+
+    public function test($post)
+    {
+
+        $args = array(
+            'post_type' => 'polylang_mo',
+            'post_status' => array('private'),
+        );
+        if($post == 'options') {
+            if(get_current_screen()->id == 'toplevel_page_woodyseo_settings') {
+                $languages = pll_languages_list();
+                $blog_name = get_field_object('field_64887499e6961');
+                $blog_description = get_field_object('field_64887519e6962');
+                $touristic_sheet = get_field_object('field_64887531e6963');
+                foreach ($languages as $lang) {
+                    $blog_name['name'] .= '_' . $lang;
+                    $blog_description['name'] .= '_' . $lang;
+                    $touristic_sheet['name'] .= '_' . $lang;
+                }
+            }
+        }
+        error_log(print_r(get_current_screen(),true),3,'/tmp/sleep1');
+        error_log(print_r(get_fields($post->ID),true),3,'/tmp/sleep');
+        // $query = new \WP_Query($args);
+        // $title = get_bloginfo('name');
+        // $description = get_bloginfo('description');
+        // $test4 = [];
+        // foreach($query->posts as $post) {
+        //     $test3 = maybe_unserialize(get_post_meta($post->ID,'',true)['_pll_strings_translations'][0]);
+        //     foreach($test3 as &$field) {
+        //         if( in_array('touristic_sheet',$field)){
+        //             $field[1] = get_field();
+        //             $new_sheet = false;
+        //         }
+        //         if( in_array($title,$field)){
+        //             $field[1] = get_field();
+        //             $new_title = false;
+        //         }
+        //         if( in_array($description,$field)){
+        //             $field[1] = get_field();
+        //             $new_description = false;
+        //         }
+        //     }
+        //     if($new_sheet) {
+        //         $test3[] = ['touristic_sheet',get_field()];
+        //     }
+        //     if($new_title) {
+        //         $test3[] = [$title,get_field()];
+        //     }
+        //     if($new_description) {
+        //         $test3[] = [$description,get_field()];
+        //     }
+        //     update_post_meta($post->ID,'_pll_strings_translations',$test3);
+        //     $test4[] = maybe_unserialize(get_post_meta($post->ID,'',true)['_pll_strings_translations'][0]);
+        // }
+    }
+
 }
