7120f1773972658d1001d2e002923e108f0ba8a6
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2023-07-24 16:19:27 +0200

    up

--- pwa/pwa.mjs
+++ pwa/pwa.mjs
@@ -1,39 +1,33 @@
-import {installServiceWorker} from 'https://api.tourism-system.rc-preprod.com/v2/notif/client.mjs';
+import {installServiceWorker, subscribeNotif, installPwa} from 'https://api.tourism-system.rc-preprod.com/v2/notif/client.mjs';
 
 if(await installServiceWorker(new URL('../woody-sw.js', import.meta.url))){
-    //TODO: Ecouter le broadcast channel pour savoir quand demander la permission d'envoyer les notifications
-    // if(await installPwa(new URL('../woody-sw.js', import.meta.url))){
-    //TODO: Afficher la modal
-
-    // };
+    console.log('Service worker is running');
+    if(await installPwa(new URL('../woody-sw.js', import.meta.url))){
+        console.log('PWA is installed');
+    }
 };
-
-
 
 const notificationPermission = new BroadcastChannel('notification_permission');
 notificationPermission.addEventListener('message', event => {
-    console.log(event);
-    //Display modal + button
-    //TODO: au clic appel demande de permission
+    displayBanner('pwaNotifBanner', subscribeNotif);
 });
 
 const installPermission = new BroadcastChannel('install_permission');
 notificationPermission.addEventListener('message', event => {
     if (window.innerWidth < 1024) {
-        console.log(event);
-        displayBanner();
+        displayBanner('pwaInstallBanner', installPwa);
     }
 });
 
-function displayBanner(){
-    let pwaInstallBanner = document.getElementById('pwaInstallBanner');
+function displayBanner(bannerID, triggerID, method){
+    let pwaBanner = document.getElementById(bannerID);
     pwaInstallBanner.classList.remove('invisible');
-    document.getElementById('closePwaInstall').addEventListener('click', closeBanner);
-    document.getElementById('triggerPwaInstall').addEventListener('click', displayBanner); display == methode jerome
-}
+    document.getElementById('closePwaInstall').addEventListener('click', function(){
+        document.getElementById(bannerID).remove();
+    });
 
-
-    function closeBanner() {
-    // mask modal
-    document.getElementById('pwaInstallBanner').remove();
+    document.getElementById(triggerID).addEventListener('click', function(){
+        document.getElementById(bannerID).remove();
+        method;
+    });
 }
