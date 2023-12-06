9095c75b7c212b8a2032c7be3d51159f623481af
Maxime Fiquet <maxime.fiquet@raccourci.fr>
2023-12-06 11:32:22 +0100

    [fix] display button geomap

--- library/classes/content/class-api-rest.php
+++ library/classes/content/class-api-rest.php
@@ -78,7 +78,8 @@ class WoodyTheme_Api_Rest
         if ($html_format && !empty($tpl_twig)) {
             return Timber::compile('cards/' . $tpl_twig . '/tpl.twig', [
                 'item' => $post_preview,
-                'image_style' => $ratio
+                'image_style' => $ratio,
+                'display_button' => empty($wrapper['display_button']) ? false : $wrapper['display_button']
             ]);
         } else {
             return $post_preview;
