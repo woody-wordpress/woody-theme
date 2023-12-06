668e1c783e742d32e80a257462a3d9599d0d4afa
Léo POIROUX <leo.poiroux@gmail.com>
2023-12-06 17:39:49 +0100

    Bugfix Datalayer with locale en_NZ + Added current_locale_region

--- library/classes/timber/class-timber.php
+++ library/classes/timber/class-timber.php
@@ -118,7 +118,26 @@ if (!class_exists('Timber')) {
             $return = [];
 
             if (!empty($vars['globals'])) {
-                $keys = ['options', 'post_title', 'post_id', 'post_image', 'post_type', 'page_type', 'sheet_id', 'woody_options_pages', 'tags', 'area', 'current_lang', 'current_locale', 'current_season', 'ancestors', 'env', 'site_key'];
+                $keys = [
+                    'ancestors',
+                    'area',
+                    'current_lang',
+                    'current_locale',
+                    'current_locale_region',
+                    'current_season',
+                    'env',
+                    'languages',
+                    'options',
+                    'page_type',
+                    'post_id',
+                    'post_image',
+                    'post_title',
+                    'post_type',
+                    'sheet_id',
+                    'site_key',
+                    'tags',
+                    'woody_options_pages',
+                ];
                 foreach ($keys as $key) {
                     if (!empty($vars['globals'][$key])) {
                         $return[$key] = $vars['globals'][$key];
@@ -136,20 +155,28 @@ if (!class_exists('Timber')) {
                 'data' => [
                     'env' => (empty($vars['globals']['env'])) ? null : $vars['globals']['env'],
                     'site_key' => (empty($vars['globals']['site_key'])) ? null : $vars['globals']['site_key'],
-                    'lang' => (empty($vars['globals']['current_locale'])) ? null : $vars['globals']['current_locale'],
-                    'season' => (empty($vars['globals']['current_season'])) ? null : $vars['globals']['current_season'],
-                    'area' => (empty($vars['globals']['area'])) ? null : $vars['globals']['area'],
                     'page' => [
                         'id_page' => (empty($vars['globals']['post_id'])) ? null : $vars['globals']['post_id'],
                         'name' => (empty($vars['globals']['post_title'])) ? null : $vars['globals']['post_title'],
                         'page_type' => (empty($vars['globals']['page_type'])) ? $vars['globals']['post_type'] : $vars['globals']['page_type'],
                         'tags' => (empty($vars['globals']['tags'])) ? null : $vars['globals']['tags'],
-                        // 'lang' => $vars['globals']['current_locale'],
-                        // 'season' => $vars['globals']['current_season'],
-                        // 'area' => $vars['globals']['area'],
                     ]
                 ]
             ];
+
+            if(!empty($vars['globals']['current_season']) && !empty($vars['globals']['current_locale'])) {
+                $datalayer['data']['lang'] = $vars['globals']['current_locale'];
+            } elseif(!empty($vars['globals']['current_lang'])) {
+                $datalayer['data']['lang'] = $vars['globals']['current_lang'];
+            }
+
+            if(!empty($vars['globals']['season'])) {
+                $datalayer['data']['season'] = $vars['globals']['season'];
+            }
+
+            if(!empty($vars['globals']['area'])) {
+                $datalayer['data']['area'] = $vars['globals']['area'];
+            }
 
             return apply_filters('woody_gtm_datalayer', $datalayer, $vars);
         }
