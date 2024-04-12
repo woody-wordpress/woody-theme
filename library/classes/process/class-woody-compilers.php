c7be85afa999e806033d9559de8c5eae2a8014f4
Dorothee Schneider <ds.lite45@gmail.com>
2024-04-12 11:43:08 +0200

    [func] enable lazyload on landswiper slide next

--- library/classes/process/class-woody-compilers.php
+++ library/classes/process/class-woody-compilers.php
@@ -889,7 +889,7 @@ class WoodyTheme_WoodyCompilers
                 }
 
                 if (!empty($slide['landswpr_slide_media']) && $slide['landswpr_slide_media']['landswpr_slide_media_type'] == 'img' && !empty($slide['landswpr_slide_media']['landswpr_slide_img'])) {
-                    $home_slider['landswpr_slides'][$slide_key]['landswpr_slide_media']['landswpr_slide_img']['lazy'] = apply_filters('woody_landswpr_slide_img_lazy', 'disabled');
+                    $home_slider['landswpr_slides'][0]['landswpr_slide_media']['landswpr_slide_img']['lazy'] = apply_filters('woody_landswpr_slide_img_lazy', 'disabled');
                     $home_slider['landswpr_slides'][$slide_key]['landswpr_slide_media']['landswpr_slide_img']['attachment_more_data'] = $this->tools->getAttachmentMoreData($home_slider['landswpr_slides'][$slide_key]['landswpr_slide_media']['landswpr_slide_img']['ID']);
                 }
 
