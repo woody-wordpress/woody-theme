5cdcc9b43af5f52ce1b6d15554fa3476170500a3
Maxime Fiquet <maxime.fiquet@raccourci.fr>
2024-04-23 10:44:55 +0200

    [FIX] escaping HTML in select field - ACF update

--- src/js/admin/acf-select2.js
+++ src/js/admin/acf-select2.js
@@ -1,14 +1,6 @@
 if (typeof(acf) !== 'undefined') {
     acf.add_filter('select2_escape_markup', function( escaped_value, original_value, $select, settings, field, instance ){
-
-        // do something to the original_value to override the default escaping, then return it.
-        // this value should still have some kind of escaping for security, but you may wish to allow specific HTML.
-        if (field.data( 'name' ) == "select2_with_html") {
-            return my_custom_escaping_method( original_value );
-        }
-
-        // return
-        return escaped_value;
-
+        // Remove HTML escaping to display html in select fields
+        return original_value;
     });
 }
