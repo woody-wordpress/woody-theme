e4d14c3cd4d8617cc901fef4c17a3f460a1f4f2d
marion.perrona <marion.perrona@raccourci.fr>
2024-07-08 17:42:01 +0200

    [TCK68951] add img mobile on landswpr

--- library/classes/process/class-woody-compilers.php
+++ library/classes/process/class-woody-compilers.php
@@ -906,6 +906,10 @@ class WoodyTheme_WoodyCompilers
                     $home_slider['landswpr_slides'][$slide_key]['landswpr_slide_media']['landswpr_slide_img']['attachment_more_data'] = $this->tools->getAttachmentMoreData($home_slider['landswpr_slides'][$slide_key]['landswpr_slide_media']['landswpr_slide_img']['ID']);
                 }
 
+                if (!empty($slide['landswpr_slide_media']) && $slide['landswpr_slide_media']['landswpr_slide_media_type'] == 'img' && !empty($slide['landswpr_slide_media']['mobile_landswpr_slide_img'])) {
+                    $home_slider['landswpr_slides'][$slide_key]['landswpr_slide_media']['mobile_landswpr_slide_img']['attachment_more_data'] = $this->tools->getAttachmentMoreData($home_slider['landswpr_slides'][$slide_key]['landswpr_slide_media']['mobile_landswpr_slide_img']['ID']);
+                }
+
                 if (!empty($slide['landswpr_slide_add_social_movie']) && !empty($slide['landswpr_slide_social_movie'])) {
                     preg_match_all('#src="([^"]+)"#', $slide['landswpr_slide_social_movie'], $result);
                     if (!empty($result[1]) && !empty($result[1][0])) {
