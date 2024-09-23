c9c9ac8eaad0ac010f24611f022eb4ee005eb68f
Orphée <orphee.besson@raccourci.fr>
2024-09-23 09:11:56 +0200

    [edit] improve

--- src/js/admin/check-balise-h1.js
+++ src/js/admin/check-balise-h1.js
@@ -1,22 +1,28 @@
 const checkBalise = () => {
-    const checkTitle = document.querySelector('[id=acf-field_5b2bbb4aec6b1]');
-    const checkTitleSwitch = checkTitle !== null ? checkTitle.nextElementSibling : '';
+    let displayAlert = false;
 
-    if (checkTitleSwitch !== null && !checkTitleSwitch.className.includes('-on')) {
-        const checkHeroTitle = document.querySelector('[id=acf-field_5b052bbab3867-field_5b052bbab3867_field_5b041d61adb72]');
-        const checkHeroImg = document.querySelector('[name="acf[field_5b0e5ddfd4b1b]"]');
-        if (checkHeroTitle !== null && (!checkHeroTitle.value || !checkHeroImg.value)) {
-            alert("Le champs Titre ou l'image du Visuel et accroche n'est pas rempli. Vous n'aurez pas de balise <h1> sur votre page. Cela impacte négativement votre SEO.");
+    const checkPageTeaserTitle = document.querySelector('[id=acf-field_5b2bbb4aec6b1]');
+    const pageTeaserTitleSwitch = checkPageTeaserTitle !== null ? checkPageTeaserTitle.nextElementSibling : '';
+
+    if (pageTeaserTitleSwitch !== null && !pageTeaserTitleSwitch.className.includes('-on')) {
+        const heroTitle = document.querySelector('[id=acf-field_5b052bbab3867-field_5b052bbab3867_field_5b041d61adb72]');
+        const heroImg = document.querySelector('[name="acf[field_5b0e5ddfd4b1b]"]');
+        if ((heroTitle !== null && !heroTitle.value) && (heroImg !== null && !heroImg.value)) {
+            displayAlert = true;
         }
+    }
+
+    if (displayAlert) {
+        alert("Le champ \"Titre\" du visuel et accroche n'est pas rempli ou \"Afficher le titre de la page\" de l'en-tête de page n'est pas coché. Vous n'aurez pas de balise <h1> sur votre page. Cela impacte négativement votre SEO.");
     }
 };
 
 if (!document.body.classList.contains('post-type-acf-field-group')) {
     if (document.getElementById('publish')) {
-        document.getElementById('publish').addEventListener('click', e => { checkBalise(); });
+        document.getElementById('publish').addEventListener('click', () => { checkBalise(); });
     }
     if (document.getElementById('save-post')) {
-        document.getElementById('save-post').addEventListener('click', e => { checkBalise(); });
+        document.getElementById('save-post').addEventListener('click', () => { checkBalise(); });
     }
 
     window.onbeforeunload = null;
