e824ef7d06e420e73954dc5fd759e725c3f0a41f
Léo POIROUX <leo.poiroux@gmail.com>
2025-02-10 15:13:57 +0100

    min jquery

--- src/lib/custom/jquery@3.7.1.min.mjs
+++ src/lib/custom/jquery@3.7.1.min.mjs
@@ -1,4 +1,1 @@
-import './jquery@3.7.1.min.js';
-const $ = window.jQuery, jQuery = window.jQuery; window.$ = window.jQuery = $;
-export { jQuery, $ };
-export default jQuery;
+import "./jquery@3.7.1.min.js"; const $ = window.jQuery, jQuery = window.jQuery; window.$ = window.jQuery = $; export { jQuery, $ }; export default jQuery;
